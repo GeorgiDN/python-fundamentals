6f7b8ecd8ec97d83c2be759f6c7bb82557452d64
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-09-13 19:54:34 +0300

    Solution to tasks from Data Types and Variables - Exercises

---
+++ data_types_and_variables_exercise/elevator.py
@@ -0,0 +1,9 @@
+capacity = int(input())
+
+if persons <= capacity:
+    print(1)
+else:
+    first_course = persons // capacity
+    if persons % capacity != 0:
+        first_course += 1
+    print(first_course)
