62978b8dad8c886ae846d9938975a8e166e5c30f
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-09-16 18:58:20 +0300

    Solution to tasks from Data Types and Variables – More Exercises

---
+++ data_types_and_variables_more_exercises/exchange_integers.py
@@ -0,0 +1,15 @@
+a = input()
+b = input()
+
+a_old = a
+b_old = b
+
+a_new = b
+b_new = a
+
+print(f'Before:\n'
+      f'a = {a_old}\n'
+      f'b = {b_old}\n'
+      f'After:\n'
+      f'a = {a_new}\n'
+      f'b = {b_new}')
