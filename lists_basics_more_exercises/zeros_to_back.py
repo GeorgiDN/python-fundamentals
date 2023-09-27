d2f0704dcb64635a111c87398fed16274c173805
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-09-27 19:44:07 +0300

    Solution to tasks from Lists Basics – More Exercises

--- lists_basics_more_exercises/zeros_to_back.py
+++ lists_basics_more_exercises/zeros_to_back.py
@@ -12,3 +12,16 @@ for num in my_list:
 
 result = non_zeros + zeros
 print(result)
+
+
+
+# advanced
+# input_string = input()
+# numbers = [int(x) for x in input_string.split(", ")]
+
+# zeros = numbers.count(0)  # Count the number of zeros in the list
+# non_zeros = [num for num in numbers if num != 0]  # Create a list of non-zero numbers
+
+# result = non_zeros + [0] * zeros  # Concatenate the non-zero list with zeros
+
+# print(result)
