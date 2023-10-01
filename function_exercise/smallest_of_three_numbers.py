f942931c7e8db9d81961c374acc3b24c96b641cc
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-01 20:33:52 +0300

    Solution to tasks from Functions - Exercise

---
+++ function_exercise/smallest_of_three_numbers.py
@@ -0,0 +1,9 @@
+def find_smallest(num1, num2, num3):
+    return min(num1, num2, num3)
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+result = find_smallest(num1, num2, num3)
+print(result)
