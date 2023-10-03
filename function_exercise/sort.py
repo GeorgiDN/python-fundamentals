a9fc010068cd48f74cd8ca341454e55721eea7c6
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-03 18:56:49 +0300

    Solution to tasks from Functions - Exercise

---
+++ function_exercise/sort.py
@@ -0,0 +1,13 @@
+number_list = list(map(int, input().split()))
+def sort():
+    sorted_list = list(sorted(number_list))
+    print(sorted_list)
+
+sort()
+
+
+# number_list = list(map(int, input().split()))
+#
+# sorted_list = list(sorted(number_list))
+#
+# print(sorted_list)
