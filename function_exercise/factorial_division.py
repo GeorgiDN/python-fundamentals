f227a7f837a490ba975c7aef12f0aaf1588d2d5c
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-05 19:32:18 +0300

    Solution to tasks from Functions - Exercise

--- function_exercise/factorial_division.py
+++ function_exercise/factorial_division.py
@@ -10,3 +10,21 @@ number_2 = int(input())
 
 result = factorial(number) / number_2
 print(f'{result:.2f}')
+
+
+
+
+# def factoriall(n):
+
+#     factoriall = 1
+
+#     for i in range(n):
+#         factoriall *= i + 1
+
+#     return factoriall
+
+# num_1 = int(input())
+# num_2 = int(input())
+
+# result = factoriall(num_1) / num_2
+# print(f'{result:.2f}')
