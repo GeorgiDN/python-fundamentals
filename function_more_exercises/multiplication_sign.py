fadda1ae9e14c58d5463e983d135169d6cdce2ca
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-07 19:36:48 +0300

    Solution to tasks from Functions – More Exercises

--- function_more_exercises/multiplication_sign.py
+++ function_more_exercises/multiplication_sign.py
@@ -12,3 +12,44 @@ num2 = int(input())
 num3 = int(input())
 
 multiplication_sign(num1, num2, num3)
+
+
+
+# def multiplication_sign(n1, n2, n3):
+#     positive_count = 0
+#     negative_count = 0
+#     zero_condition = False
+
+#     if n1 > 0:
+#         positive_count += 1
+#     elif n1 < 0:
+#         negative_count += 1
+#     else:
+#         zero_condition = True
+#     if n2 > 0:
+#         positive_count += 1
+#     elif n2 < 0:
+#         negative_count += 1
+#     else:
+#         zero_condition = True
+#     if n3 > 0:
+#         positive_count += 1
+#     elif n3 < 0:
+#         negative_count += 1
+#     else:
+#         zero_condition = True
+
+#     if not zero_condition:
+#         if negative_count % 2 == 0:
+#             print('positive')
+#         elif negative_count % 2 != 0:
+#             print('negative')
+#     else:
+#         print('zero')
+
+
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+
+# multiplication_sign(num1, num2, num3)
