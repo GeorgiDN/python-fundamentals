03031fbf013d27c1286fd37574384e995a6a7523
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-15 20:10:31 +0300

    Solution to tasks from List Advanced - Lab

--- lists_advanced_lab/4_palindrome_string.py
+++ lists_advanced_lab/4_palindrome_string.py
@@ -14,3 +14,22 @@ for i in words:
 print(palindromes)
 print(f"Found palindrome {count_palindromes} times")
 
+
+
+# def palindrome(string, palindrome_count):
+#     palindromes = []
+#     palindrome_count = 0
+
+#     for i in string:
+#         if i == i[::-1]:
+#             palindromes.append(i)
+#             if i == searched_palindrome:
+#                 palindrome_count += 1
+
+#     print(palindromes)
+#     print(f"Found palindrome {palindrome_count} times")
+
+# words = input().split(' ')
+# searched_palindrome = input()
+
+# palindrome(words, searched_palindrome)
