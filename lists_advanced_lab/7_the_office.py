28f3f497bbfcec8f8a3c398bfb1bb10d0ac861f5
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-16 19:40:46 +0300

    Solution to tasks from List Advanced - Lab

--- lists_advanced_lab/7_the_office.py
+++ lists_advanced_lab/7_the_office.py
@@ -9,3 +9,17 @@ total_count = len(improved_happiness)
 message = 'happy' if happy_count >= total_count / 2 else 'not happy'
 
 print(f'Score: {happy_count}/{total_count}. Employees are {message}!')
+
+
+
+
+# employees = input().split((' '))
+# happiness_factor = int(input())
+# employees = list(map(lambda x: int(x) * happiness_factor, employees))
+
+# filtered = list(filter(lambda x: x >= (sum(employees) / len(employees)), employees))
+
+# if len(filtered) >= len(employees) / 2:
+#     print(f"Score: {len(filtered)}/{len(employees)}. Employees are happy!")
+# else:
+#     print(f"Score: {len(filtered)}/{len(employees)}. Employees are not happy!")
