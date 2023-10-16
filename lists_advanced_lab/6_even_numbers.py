78552652cbabdae39bb21febf4913e0288cb17b5
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-16 19:40:07 +0300

    Solution to tasks from List Advanced - Lab

--- lists_advanced_lab/6_even_numbers.py
+++ lists_advanced_lab/6_even_numbers.py
@@ -7,3 +7,14 @@ for index, num in enumerate(number_list):
         even_indices.append(index)
 
 print(even_indices)
+
+
+
+# number_list = list(map(int, input().split(', ')))
+
+# found_indices_or_no = map(lambda x: x if number_list[x] % 2 == 0 else 'no',
+#                           range(len(number_list)))
+
+
+# even_indices = list(filter(lambda a: a != 'no', found_indices_or_no))
+# print(even_indices)
