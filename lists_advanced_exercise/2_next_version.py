5cfd715f41791c4afc47bef03253deddd0c9bd9a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-17 19:13:33 +0300

    Solution to tasks from List Advanced - Exercise

--- lists_advanced_exercise/2_next_version.py
+++ lists_advanced_exercise/2_next_version.py
@@ -9,3 +9,20 @@ else:
     next_version = f'{version[0] + 1}.0.0'
 
 print(next_version)
+
+
+
+# version = list(map(int, input().split('.')))
+
+# version[2] += 1
+
+# if version[2] > 9:
+#     version[2] = 0
+#     version[1] += 1
+
+# if version[1] > 9:
+#     version[1] = 0
+#     version[0] += 1
+
+# next_version = '.'.join(map(str, version))
+# print(next_version)
