dcfef483aad130c03ae7e87ed527480e0da4ae68
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-20 19:56:16 +0300

    Solution to tasks from List Advanced - More Exercises

---
+++ lists_advanced_more_exercises/2_take_skip_rope.py
@@ -0,0 +1,31 @@
+input_string = input()
+input_list = list(input_string)
+
+number_list = []
+non_number_list = []
+
+for char in input_list:
+    if char.isdigit():
+        number_list.append(char)
+    else:
+        non_number_list.append(char)
+
+number_list = list(map(int, number_list))  # all numbers
+take_list = []  
+skip_list = []  
+
+for index, num in enumerate(number_list):
+    if index % 2 == 0:
+        take_list.append(num)
+    else:
+        skip_list.append(num)
+
+taken_string = []
+
+for i in range(len(take_list)):
+    taken_characters = non_number_list[:take_list[i]]
+    non_number_list = non_number_list[take_list[i] + skip_list[i]:]
+    taken_string.extend(taken_characters)
+
+final_result = ''.join(taken_string)
+print(final_result)
