8c50b0ceecb7499cd0cfaa08b3a0e2097b8d96af
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-10-26 19:01:54 +0300

    Solution to tasks Dictionaries - Lab

--- dictionaries_lab/1_bakery.py
+++ dictionaries_lab/1_bakery.py
@@ -4,3 +4,16 @@ values = elements[1::2]
 bakery = dict(zip(keys, map(int, values)))
 
 print(bakery)
+
+
+
+
+# elements = input().split(" ")
+# bakery = {}
+#
+# for i in range(0, len(elements), 2):
+#     key = elements[i]
+#     value = elements[i + 1]
+#     bakery[key] = int(value)
+#
+# print(bakery)
