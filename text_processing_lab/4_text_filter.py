662444cba7df0c626ee87ca2bd4ee37dc2b1c8f6
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-11-04 22:22:16 +0200

    Update 4_text_filter.py

--- text_processing_lab/4_text_filter.py
+++ text_processing_lab/4_text_filter.py
@@ -8,3 +8,11 @@ print(text)
 
 
 
+# banned_words = input().split(', ')
+# text = input()
+#
+# for word in banned_words:
+#     while word in text:
+#         text = text.replace(word, '*' * len(word))
+#
+# print(text)
