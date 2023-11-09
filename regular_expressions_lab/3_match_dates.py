3d17a1e7d9c6838c2ef8cbee630fb7ba5b42606d
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-11-09 19:17:44 +0200

    Update 3_match_dates.py

--- regular_expressions_lab/3_match_dates.py
+++ regular_expressions_lab/3_match_dates.py
@@ -11,3 +11,14 @@ for match in matches:
     month = match.group(3)
     year = match.group(4)
     print(f'Day: {day}, Month: {month}, Year: {year}')
+
+
+
+
+# import re
+# pattern = r"\b(?P<day>\d{2})([-./])(?P<month>[A-Z][a-z]{2})\2(?P<year>\d{4})\b"
+# text = input()
+# matches = re.finditer(pattern, text)
+#
+# for match in matches:
+#     print(f'Day: {match["day"]}, Month: {match["month"]}, Year: {match["year"]}')
