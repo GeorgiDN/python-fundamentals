a843a01183bcbb7010a8e321718fd7832295c570
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-10 21:59:49 +0200

    Update factorial_division.py

--- function_exercise/factorial_division.py
+++ function_exercise/factorial_division.py
@@ -1,30 +1,16 @@
+def factorial_division(first_num, second_num):
+    return round(factorial(first_num) / factorial(second_num), 2)
+
 def factorial(num):
-    sum_of_multiplies = 1
-    if num > 0:
-        for i in range(1, num + 1):
-            sum_of_multiplies *= i
-    return sum_of_multiplies
-
-number = int(input())
-number_2 = int(input())
-
-result = factorial(number) / number_2
-print(f'{result:.2f}')
+    factorial_result = 1
+    for i in range(1, num + 1):
+        factorial_result *= i
+    return factorial_result
 
 
+first_num = int(input())
+second_num = int(input())
 
+result = factorial_division(first_num, second_num)
+print(f"{result:.2f}")
 
-# def factoriall(n):
-
-#     factoriall = 1
-
-#     for i in range(n):
-#         factoriall *= i + 1
-
-#     return factoriall
-
-# num_1 = int(input())
-# num_2 = int(input())
-
-# result = factoriall(num_1) / num_2
-# print(f'{result:.2f}')
