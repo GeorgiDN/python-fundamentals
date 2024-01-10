7bf12e251df50bcb8490fcdcc2ec53842aa10ca4
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-10 22:02:14 +0200

    Update 5_class.py

--- object_and_casses_exercise/5_class.py
+++ object_and_casses_exercise/5_class.py
@@ -5,23 +5,16 @@ class Class:
         self.name = name
         self.students = []
         self.grades = []
+        self.average_grade = 0
 
-    def add_student(self, name: str, grade: float):
-        if Class.__students_count > 0:
+    def add_student(self, name, grade):
+        if len(self.students) < Class.__students_count:
             self.students.append(name)
             self.grades.append(grade)
-            Class.__students_count -= 1
 
-    def get_average_grade(self,):
-        return f"{sum(self.grades) / len(self.grades):.2f}"
-
+    def get_average_grade(self):
+        self.average_grade = sum(self.grades) / len(self.grades)
+        return round(self.average_grade, 2)
 
     def __repr__(self):
         return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {self.get_average_grade()}"
-
-
-a_class = Class("11B")
-a_class.add_student("Peter", 4.80)
-a_class.add_student("George", 6.00)
-a_class.add_student("Amy", 3.50)
-print(a_class)
