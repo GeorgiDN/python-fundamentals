e3d8e1318f51fd3deefe39a52f54a5ae26bdbc06
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-11 19:41:25 +0200

    Update balanced_brackets.py

--- data_types_and_variables_more_exercises/balanced_brackets.py
+++ data_types_and_variables_more_exercises/balanced_brackets.py
@@ -1,21 +1,31 @@
-lines = int(input())
-
-balance = 0
-
-for _ in range(lines):
-    expression = input()
-
-    if expression == '(':
-        balance += 1
-    elif expression == ')':
-        balance -= 1
-
-    if balance < 0:
-        print('UNBALANCED')
-        break
-
-if balance == 0:
-    print('BALANCED')
-elif balance > 0:
-    print('UNBALANCED')
-    
+number_of_lines = int(input())
+open_bracket = False
+closed_bracket = False
+parentheses_are_balanced = False
+for _ in range(number_of_lines):
+    symbol = input()
+    if symbol == "(":
+        if not open_bracket:
+            open_bracket = True
+        else:
+            parentheses_are_balanced = False
+            break
+    elif symbol == ")":
+        if not open_bracket:
+            parentheses_are_balanced = False
+            break
+        elif not closed_bracket:
+            closed_bracket = True
+            if open_bracket and closed_bracket:
+                closed_bracket = False
+                open_bracket = False
+                parentheses_are_balanced = True
+            else:
+                parentheses_are_balanced = False
+        else:
+            parentheses_are_balanced = False
+            break
+if parentheses_are_balanced:
+    print("BALANCED")
+else:
+    print("UNBALANCED")
