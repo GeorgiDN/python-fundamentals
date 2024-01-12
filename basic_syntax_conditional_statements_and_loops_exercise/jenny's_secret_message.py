088e921578df7f95313dce637a5cd5c4cd4d1e9a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-12 19:41:43 +0200

    Update jenny's_secret_message.py

--- basic_syntax_conditional_statements_and_loops_exercise/jenny's_secret_message.py
+++ basic_syntax_conditional_statements_and_loops_exercise/jenny's_secret_message.py
@@ -1,6 +1,11 @@
-name = input()
+# name = input()
 
-if name == 'Johnny':
-    print('Hello, my love!')
-else:
-    print(f"Hello, {name}!")
+# if name == 'Johnny':
+#     print('Hello, my love!')
+# else:
+#     print(f"Hello, {name}!")
+
+
+
+name = input()
+print('Hello, my love!' if name == "Johnny" else f"Hello, {name}!")
