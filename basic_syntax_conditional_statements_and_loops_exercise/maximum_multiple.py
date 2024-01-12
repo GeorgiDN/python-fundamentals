f6ee6000538d39f83e2279f3217cf04a1df804a3
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-12 19:58:35 +0200

    Update maximum_multiple.py

--- basic_syntax_conditional_statements_and_loops_exercise/maximum_multiple.py
+++ basic_syntax_conditional_statements_and_loops_exercise/maximum_multiple.py
@@ -1,9 +1,9 @@
 def maximum_multiple(div, bound):
-    largest_num = 0
-    for i in range(div, bound + 1):
-        if i % div == 0:
-            largest_num = i
-    return largest_num
+    largest_divisible_number = 0
+    for number in range(div, bound + 1):
+        if number % div == 0:
+            largest_divisible_number = number
+    return largest_divisible_number
 
 
 divisor = int(input())
