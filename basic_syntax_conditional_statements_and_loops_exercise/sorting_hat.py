f76223f75560de8179f7ad97e11712f80b6956de
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-12 21:57:05 +0200

    Update sorting_hat.py

--- basic_syntax_conditional_statements_and_loops_exercise/sorting_hat.py
+++ basic_syntax_conditional_statements_and_loops_exercise/sorting_hat.py
@@ -1,23 +1,42 @@
-name = input()
-char = 0
+def sorting_hat():
+    while True:
+        name = input()
+        if name == "Welcome!":
+            return print("Welcome to Hogwarts.")
+        if name == "Voldemort":
+            return print("You must not speak of that name!")
+        elif len(name) < 5:
+            print(f"{name} goes to Gryffindor.")
+        elif len(name) == 5:
+            print(f"{name} goes to Slytherin.")
+        elif len(name) == 6:
+            print(f"{name} goes to Ravenclaw.")
+        elif len(name) > 6:
+            print(f"{name} goes to Hufflepuff.")
 
-while name != 'Welcome!':
-    if name == 'Voldemort':
-        print('You must not speak of that name!')
-        break
-    for i in name:
-        char += 1
-    if char < 5:
-        print(f"{name} goes to Gryffindor.")
-    elif char == 5:
-        print(f"{name} goes to Slytherin.")
-    elif char == 6:
-        print(f"{name} goes to Ravenclaw.")
-    elif char > 6:
-        print(f"{name} goes to Hufflepuff.")
 
-    char = 0
-    name = input()
+sorting_hat()
 
-if name == 'Welcome!':
-    print('Welcome to Hogwarts.')
+# name = input()
+# char = 0
+
+# while name != 'Welcome!':
+#     if name == 'Voldemort':
+#         print('You must not speak of that name!')
+#         break
+#     for i in name:
+#         char += 1
+#     if char < 5:
+#         print(f"{name} goes to Gryffindor.")
+#     elif char == 5:
+#         print(f"{name} goes to Slytherin.")
+#     elif char == 6:
+#         print(f"{name} goes to Ravenclaw.")
+#     elif char > 6:
+#         print(f"{name} goes to Hufflepuff.")
+
+#     char = 0
+#     name = input()
+
+# if name == 'Welcome!':
+#     print('Welcome to Hogwarts.')
