00214c486eab4ba8fbe70b3ffb0fef8048b71fcc
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-12 19:43:19 +0200

    Update drink_something.py

--- basic_syntax_conditional_statements_and_loops_exercise/drink_something.py
+++ basic_syntax_conditional_statements_and_loops_exercise/drink_something.py
@@ -1,21 +1,43 @@
-ages = {"drink toddy": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-        "drink coke": [15, 16, 17, 18],
-        "drink beer": [19, 20, 21]
-        }
+def get_drink(age):
+    age_ranges = {
+        (0, 14): 'drink toddy',
+        (15, 18): 'drink coke',
+        (19, 21): 'drink beer',
+        (22, float('inf')): 'drink whisky'
+    }
 
-age = int(input())
-if age in ages["drink toddy"]:
-    print("drink toddy")
-elif age in ages["drink coke"]:
-    print("drink coke")
-elif age in ages["drink beer"]:
-    print("drink beer")
-else:
-    print('drink whisky')
+    for age_range, drink in age_ranges.items():
+        if age_range[0] <= age <= age_range[1]:
+            return drink
 
 
+current_age = int(input())
+result = get_drink(current_age)
+print(result)
 
 
+
+
+
+###
+# ages = {"drink toddy": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+#         "drink coke": [15, 16, 17, 18],
+#         "drink beer": [19, 20, 21]
+#         }
+
+# age = int(input())
+# if age in ages["drink toddy"]:
+#     print("drink toddy")
+# elif age in ages["drink coke"]:
+#     print("drink coke")
+# elif age in ages["drink beer"]:
+#     print("drink beer")
+# else:
+#     print('drink whisky')
+
+
+
+###
 # age = int(input())
 
 # if age <= 14:
