776fdb08fd4abfada84e0f481785aa6dd5ee9142
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-14 21:44:14 +0200

    Update statistics.py

--- lists_basics_lab/statistics.py
+++ lists_basics_lab/statistics.py
@@ -1,14 +1,65 @@
-lines = int(input())
+def take_int_input():
+      return int(input())
 
-numbers = [int(input()) for _ in range(lines)]
-my_positive_list = [num for num in numbers if num >= 0]
-my_negative_list = [num for num in numbers if num < 0]
 
-print(f'{my_positive_list}\n'
-      f'{my_negative_list}')
-print(f"Count of positives: {len(my_positive_list)}\n"
-      f"Sum of negatives: {sum(my_negative_list)}")
+def take_numbers(lines):
+      numbers = [take_int_input() for _ in range(lines)]
+      return numbers
 
+
+def find_positive_numbers(nums):
+      positive_list = [num for num in nums if num >= 0]
+      return positive_list
+
+
+def find_negative_numbers(nums):
+      negative_list = [num for num in nums if num < 0]
+      return negative_list
+
+
+def counts_of_positives(lst):
+      return len(lst)
+
+
+def sum_negatives(lst):
+      return sum(lst)
+
+
+def print_result(pos_list, neg_list):
+      result = ""
+      result += f'{pos_list}\n' \
+                f'{neg_list}\n' \
+                f'Count of positives: {counts_of_positives(pos_list)}\n' \
+                f'Sum of negatives: {sum_negatives(neg_list)}'
+
+      return print(result)
+
+
+rows = take_int_input()
+numbers = take_numbers(rows)
+my_positive_list = find_positive_numbers(numbers)
+my_negative_list = find_negative_numbers(numbers)
+print_result(my_positive_list, my_negative_list)
+
+
+
+
+##
+# lines = int(input())
+
+# numbers = [int(input()) for _ in range(lines)]
+# my_positive_list = [num for num in numbers if num >= 0]
+# my_negative_list = [num for num in numbers if num < 0]
+
+# print(f'{my_positive_list}\n'
+#       f'{my_negative_list}')
+# print(f"Count of positives: {len(my_positive_list)}\n"
+#       f"Sum of negatives: {sum(my_negative_list)}")
+
+
+
+
+##
 # lines = int(input())
 
 # my_positive_list = []
