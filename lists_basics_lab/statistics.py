040af31f3fd44595ddbc0426c0a49ca0c9fb6be8
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-14 21:19:27 +0200

    Update statistics.py

--- lists_basics_lab/statistics.py
+++ lists_basics_lab/statistics.py
@@ -1,19 +1,30 @@
 lines = int(input())
 
-my_positive_list = []
-my_negative_list = []
-
-for i in range(lines):
-    number = int(input())
-    if number >= 0:
-        my_positive_list.append(number)
-    else:
-        my_negative_list.append(number)
-
-length_positive_list = len(my_positive_list)
-length_negative_list = sum(my_negative_list)
+numbers = [int(input()) for _ in range(lines)]
+my_positive_list = [num for num in numbers if num >= 0]
+my_negative_list = [num for num in numbers if num < 0]
 
 print(f'{my_positive_list}\n'
       f'{my_negative_list}')
-print(f"Count of positives: {length_positive_list}\n"
-      f"Sum of negatives: {length_negative_list}")
+print(f"Count of positives: {len(my_positive_list)}\n"
+      f"Sum of negatives: {sum(my_negative_list)}")
+
+# lines = int(input())
+
+# my_positive_list = []
+# my_negative_list = []
+
+# for i in range(lines):
+#     number = int(input())
+#     if number >= 0:
+#         my_positive_list.append(number)
+#     else:
+#         my_negative_list.append(number)
+
+# length_positive_list = len(my_positive_list)
+# length_negative_list = sum(my_negative_list)
+
+# print(f'{my_positive_list}\n'
+#       f'{my_negative_list}')
+# print(f"Count of positives: {length_positive_list}\n"
+#       f"Sum of negatives: {length_negative_list}")
