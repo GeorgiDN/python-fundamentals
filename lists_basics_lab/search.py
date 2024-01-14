4337c3c67f3f8442716893cac59525f590987cfe
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-14 22:33:15 +0200

    Update search.py

--- lists_basics_lab/search.py
+++ lists_basics_lab/search.py
@@ -1,17 +1,29 @@
 n = int(input())
 word = input()
 
-strings = []
-
-for i in range(n):
-    curr_string = input()
-    strings.append(curr_string)
-
+strings = [input() for _ in range(n)]
 print(strings)
 
-for i in range(len(strings)-1, -1, -1):
-    element = strings[i]
-    if word not in element:
-        strings.remove(element)
-
+strings = [strings[i] for i in range(len(strings)) if word in strings[i]]
 print(strings)
+
+
+
+
+# n = int(input())
+# word = input()
+
+# strings = []
+
+# for i in range(n):
+#     curr_string = input()
+#     strings.append(curr_string)
+
+# print(strings)
+
+# for i in range(len(strings)-1, -1, -1):
+#     element = strings[i]
+#     if word not in element:
+#         strings.remove(element)
+
+# print(strings)
