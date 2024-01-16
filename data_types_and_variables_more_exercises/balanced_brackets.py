abb2699cd07a00f807b54c78b8e497ca86bafd77
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-16 20:42:32 +0200

    Update balanced_brackets.py

--- data_types_and_variables_more_exercises/balanced_brackets.py
+++ data_types_and_variables_more_exercises/balanced_brackets.py
@@ -6,28 +6,47 @@ def take_int_input():
     return int(input())
 
 
-def increase_balance(bal):
-    return bal + 1
+def increase_balance(balance_):
+    return balance_ + 1
 
 
-def reduce_balance(bal):
-    return bal - 1
+def reduce_balance(balance_):
+    return balance_ - 1
+
+
+def fill_the_list_and_change_balance(bracket, open_br, close_br, balance_):
+    if bracket == '(':
+        open_br.append(bracket)
+        balance_ = increase_balance(balance_)
+        return open_br, close_br, balance_
+
+    elif bracket == ')':
+        close_br.append(bracket)
+        balance_ = reduce_balance(balance_)
+        return open_br, close_br, balance_
+
+    return open_br, close_br, balance_
 
 
 def check_balanced_brackets(lines):
+    open_brackets, close_brackets = [], []
     balanced = True
     balance = 0
     for _ in range(lines):
         expression = take_string_input()
 
-        if expression == '(':
-            balance = increase_balance(balance)
-        elif expression == ')':
-            balance = reduce_balance(balance)
+        open_brackets, close_brackets, balance = fill_the_list_and_change_balance(
+            expression, open_brackets, close_brackets, balance
+        )
 
         if balance > 1 or balance < 0:
             balanced = False
             break
+
+    # Second check
+    if balanced:
+        if len(open_brackets) != len(close_brackets):
+            balanced = False
 
     if balanced:
         return print('BALANCED')
@@ -40,22 +59,32 @@ check_balanced_brackets(number_of_lines)
 
 
 
+
+##
 # lines = int(input())
+# open_brackets, close_brackets = [], []
 # balanced = True
 # balance = 0
-
+#
 # for _ in range(lines):
 #     expression = input()
-
+#
 #     if expression == '(':
+#         open_brackets.append(expression)
 #         balance += 1
 #     elif expression == ')':
+#         close_brackets.append(expression)
 #         balance -= 1
-
+#
 #     if balance > 1 or balance < 0:
 #         balanced = False
 #         break
-
+#
+# # Second check
+# if balanced:
+#     if len(open_brackets) != len(close_brackets):
+#         balanced = False
+#
 # if balanced:
 #     print('BALANCED')
 # else:
@@ -64,6 +93,7 @@ check_balanced_brackets(number_of_lines)
 
 
 
+##
 # number_of_lines = int(input())
 # open_bracket = False
 # closed_bracket = False
