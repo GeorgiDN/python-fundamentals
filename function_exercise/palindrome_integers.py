dab4d838616a74db5bae00a9f543e20cc8ddea8a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-17 22:01:20 +0200

    Update palindrome_integers.py

--- function_exercise/palindrome_integers.py
+++ function_exercise/palindrome_integers.py
@@ -1,12 +1,17 @@
-number_list = list(map(int, input().split(', ')))
-
 def is_palindrome(n):
     num_str = str(n)
     reversed_str = num_str[::-1]
     return num_str == reversed_str
 
-for num in number_list:
-    if is_palindrome(num):
-        print('True')
-    else:
-        print('False')
+
+number_list = list(map(int, input().split(', ')))
+
+result = '\n'.join(str(is_palindrome(num)) for num in number_list)
+print(result)
+
+
+# for num in number_list:
+#     if is_palindrome(num):
+#         print('True')
+#     else:
+#         print('False')
