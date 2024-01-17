86c0496df953ed266b56271831f718482b9a8fdc
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-17 22:01:01 +0200

    Update min_max_and_sum.py

--- function_exercise/min_max_and_sum.py
+++ function_exercise/min_max_and_sum.py
@@ -1,6 +1,4 @@
-number_list = list(map(int, input().split()))
-
-def min_max_sum():
+def min_max_sum(number_list):
     min_number = min(number_list)
     max_number = max(number_list)
     sum_number = sum(number_list)
@@ -9,4 +7,6 @@ def min_max_sum():
           f"The maximum number is {max_number}\n"
           f"The sum number is: {sum_number}")
 
-min_max_sum()
+
+list_with_numbers = list(map(int, input().split()))
+min_max_sum(list_with_numbers)
