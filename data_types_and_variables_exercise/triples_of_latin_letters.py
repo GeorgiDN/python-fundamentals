3b4871fcd27b6f7029a6749f297b1e59464a4321
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-19 21:23:04 +0200

    Update triples_of_latin_letters.py

--- data_types_and_variables_exercise/triples_of_latin_letters.py
+++ data_types_and_variables_exercise/triples_of_latin_letters.py
@@ -1,6 +1,14 @@
-num = int(input())
+number_of_symbols = int(input())
+field = [[[f'{chr(97 + i)}{chr(97 + j)}{chr(97 + k)}\n' for k in range(number_of_symbols)] for j in range(number_of_symbols)] for i in range(number_of_symbols)]
 
-for i in range(num):
-    for j in range(num):
-        for k in range(num):
-            print(f'{chr(97 + i)}{chr(97 + j)}{chr(97 + k)}')
+result = "".join("".join("".join(matrix) for matrix in row) for row in field)
+print(result)
+
+
+
+# num = int(input())
+
+# for i in range(num):
+#     for j in range(num):
+#         for k in range(num):
+#             print(f'{chr(97 + i)}{chr(97 + j)}{chr(97 + k)}')
