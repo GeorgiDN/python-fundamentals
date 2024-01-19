15c1579985adf1a5b44e807ad3cc30ff07465dd5
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-19 21:22:18 +0200

    Update print_part_of_the_ascii_table.py

--- data_types_and_variables_exercise/print_part_of_the_ascii_table.py
+++ data_types_and_variables_exercise/print_part_of_the_ascii_table.py
@@ -1,6 +1,16 @@
 first_num = int(input())
 sec_num = int(input())
 
-for i in range(first_num, sec_num + 1):
-    print((chr(i)), end=' ')
-  
+print(*[chr(i) for i in range(first_num, sec_num + 1)])
+
+
+##
+# first_num = int(input())
+# sec_num = int(input())
+# for i in range(first_num, sec_num + 1):
+#     print((chr(i)), end=' ')
+
+
+##
+# print(*[chr(i) for i in range(int(input()), int(input()) + 1)])
+
