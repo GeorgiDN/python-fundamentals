fbf0deee2ff52f1022bc64bc423b987413e9b067
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-19 21:26:38 +0200

    Update decrypting_messages.py

--- data_types_and_variables_more_exercises/decrypting_messages.py
+++ data_types_and_variables_more_exercises/decrypting_messages.py
@@ -6,16 +6,38 @@ def decrypt_message(key, lines):
             decrypted_message += decrypted_char
     return decrypted_message
 
-# Въвеждане на ключа и броя на редовете
+
 key = int(input())
 n = int(input())
 
-# Въвеждане на редовете
+
 lines = []
 for _ in range(n):
     line = input()
     lines.append(line)
 
-# Извикване на функцията и отпечатване на резултата
+
 result = decrypt_message(key, lines)
 print(result)
+
+
+
+
+
+
+
+# key = int(input())
+# n = int(input())
+# lines = []
+
+# for _ in range(n):
+#     line = input()
+#     lines.append(line)
+
+# decrypted_message = ""
+# for line in lines:
+#     for char in line:
+#         decrypted_char = chr(ord(char) + key)
+#         decrypted_message += decrypted_char
+
+# print(decrypted_message)
