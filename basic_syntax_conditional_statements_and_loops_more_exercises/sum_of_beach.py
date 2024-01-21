f3b1be4819703cea66b6e365a0e4f2053d0997f3
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-21 22:10:43 +0200

    Update sum_of_beach.py

--- basic_syntax_conditional_statements_and_loops_more_exercises/sum_of_beach.py
+++ basic_syntax_conditional_statements_and_loops_more_exercises/sum_of_beach.py
@@ -1,13 +1,16 @@
-input_string = input()
-
-input_string = input_string.lower()
-
-sand_count = input_string.count("sand")
-water_count = input_string.count("water")
-fish_count = input_string.count("fish")
-sun_count = input_string.count("sun")
+input_str = input().lower()
+sand, water, fish, sun = input_str.count("sand"), input_str.count("water"), input_str.count("fish"), input_str.count("sun")
+print(sand + water + fish + sun)
 
 
-total_count = sand_count + water_count + fish_count + sun_count
 
-print(total_count)
+
+# input_string = input()
+# input_string = input_string.lower()
+# sand_count = input_string.count("sand")
+# water_count = input_string.count("water")
+# fish_count = input_string.count("fish")
+# sun_count = input_string.count("sun")
+# total_count = sand_count + water_count + fish_count + sun_count
+
+# print(total_count)
