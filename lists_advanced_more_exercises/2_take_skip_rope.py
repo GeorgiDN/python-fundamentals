abb2ee90becda7ea8ceb99336289d771b8901362
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-21 16:33:31 +0200

    Update 2_take_skip_rope.py

--- lists_advanced_more_exercises/2_take_skip_rope.py
+++ lists_advanced_more_exercises/2_take_skip_rope.py
@@ -1,31 +1,49 @@
-input_string = input()
-input_list = list(input_string)
+text = list(input())
 
-number_list = []
-non_number_list = []
+number_list = [int(char) for char in text if char.isdigit()]
+non_number_list = [char for char in text if not char.isdigit()]
 
-for char in input_list:
-    if char.isdigit():
-        number_list.append(char)
-    else:
-        non_number_list.append(char)
-
-number_list = list(map(int, number_list))  # all numbers
-take_list = []  
-skip_list = []  
-
-for index, num in enumerate(number_list):
-    if index % 2 == 0:
-        take_list.append(num)
-    else:
-        skip_list.append(num)
-
-taken_string = []
+take_list = [number for idx, number in enumerate(number_list) if idx % 2 == 0]
+skip_list = [number for idx, number in enumerate(number_list) if idx % 2 != 0]
+result = []
 
 for i in range(len(take_list)):
     taken_characters = non_number_list[:take_list[i]]
     non_number_list = non_number_list[take_list[i] + skip_list[i]:]
-    taken_string.extend(taken_characters)
+    result.extend(taken_characters)
 
-final_result = ''.join(taken_string)
+final_result = ''.join(result)
 print(final_result)
+
+
+# input_string = input()
+# input_list = list(input_string)
+
+# number_list = []
+# non_number_list = []
+
+# for char in input_list:
+#     if char.isdigit():
+#         number_list.append(char)
+#     else:
+#         non_number_list.append(char)
+
+# number_list = list(map(int, number_list))  # all numbers
+# take_list = []  
+# skip_list = []  
+
+# for index, num in enumerate(number_list):
+#     if index % 2 == 0:
+#         take_list.append(num)
+#     else:
+#         skip_list.append(num)
+
+# taken_string = []
+
+# for i in range(len(take_list)):
+#     taken_characters = non_number_list[:take_list[i]]
+#     non_number_list = non_number_list[take_list[i] + skip_list[i]:]
+#     taken_string.extend(taken_characters)
+
+# final_result = ''.join(taken_string)
+# print(final_result)
