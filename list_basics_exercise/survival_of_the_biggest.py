5a574b838f72e51de189a8e45af68fbfce4790a1
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-24 22:06:29 +0200

    Update survival_of_the_biggest.py

--- list_basics_exercise/survival_of_the_biggest.py
+++ list_basics_exercise/survival_of_the_biggest.py
@@ -1,10 +1,20 @@
-list_of_numbers = input().split()
-n = int(input())
+list_with_numbers = [int(x) for x in input().split(" ")]
+number_for_remove = int(input())
 
-list_numbers = [int(num) for num in list_of_numbers]
+for _ in range(number_for_remove):
+    index_min_number = list_with_numbers.index(min(list_with_numbers))
+    list_with_numbers.pop(index_min_number)
 
-for i in range(n):
-    min_value = min(list_numbers)
-    list_numbers.remove(min_value)
+print(', '.join(map(str, list_with_numbers)))
 
-print(", ".join(map(str, list_numbers)))
+
+# list_of_numbers = input().split()
+# n = int(input())
+
+# list_numbers = [int(num) for num in list_of_numbers]
+
+# for i in range(n):
+#     min_value = min(list_numbers)
+#     list_numbers.remove(min_value)
+
+# print(", ".join(map(str, list_numbers)))
