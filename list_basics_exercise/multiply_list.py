6218868db0e98f445727f76e8562d09bd8134c9f
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-24 22:02:48 +0200

    Update multiply_list.py

--- list_basics_exercise/multiply_list.py
+++ list_basics_exercise/multiply_list.py
@@ -1,11 +1,19 @@
 factor = int(input())
 count = int(input())
+list_numbers = [number * factor for number in range(1, count + 1)]
+print(list_numbers)
 
-calculated_result = []
 
-num = factor
-while len(calculated_result) < count:
-    calculated_result.append(num)
-    num += factor
 
-print(calculated_result)
+
+# factor = int(input())
+# count = int(input())
+
+# calculated_result = []
+
+# num = factor
+# while len(calculated_result) < count:
+#     calculated_result.append(num)
+#     num += factor
+
+# print(calculated_result)
