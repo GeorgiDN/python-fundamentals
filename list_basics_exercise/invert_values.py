09352eeef02805b24ace2e68436799a7452c472b
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-24 22:01:35 +0200

    Update invert_values.py

--- list_basics_exercise/invert_values.py
+++ list_basics_exercise/invert_values.py
@@ -3,16 +3,19 @@ negative_numbers = [-number for number in positive_numbers]
 print(negative_numbers)
 
 
+
+# positive_numbers = [int(x) for x in input().split(" ")]
+# print([-number for number in positive_numbers])
+
+
+
 # string = input()
-
-# my_list = string.split(' ')
-
-# my_list = [int(num) for num in my_list]
-
-# changed_result = []
-
-# for digit in my_list:
-#     reversed_digit = digit * (-1)
-#     changed_result.append(reversed_digit)
-
-# print(changed_result)
+# positive_numbers = string.split(' ')
+# positive_numbers = [int(num) for num in positive_numbers]
+# negative_numbers = []
+#
+# for number in positive_numbers:
+#     negative_digit = number * (-1)
+#     negative_numbers.append(negative_digit)
+#
+# print(negative_numbers)
