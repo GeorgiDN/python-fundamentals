ad8a567ae9f44c7dd1cec12feb1eea161dfa4430
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-25 21:22:38 +0200

    Update odd_and_even_sum.py

--- function_exercise/odd_and_even_sum.py
+++ function_exercise/odd_and_even_sum.py
@@ -1,14 +1,27 @@
-def sum_all(numbers):
-    even_sum = 0
-    odd_sum = 0
-    for num in numbers:
-        if num % 2 == 0:
-            even_sum += num
-        else:
-            odd_sum += num
-    print(f"Odd sum = {odd_sum}, Even sum = {even_sum}")
+def odd_even_sum(lst):
+    evens = sum([num for num in lst if num % 2 == 0])
+    odds = sum([num for num in lst if num % 2 != 0])
+    return evens, odds
 
 
-numbers_input = input()
-number_list = [int(digit) for digit in str(numbers_input)]
-sum_all(number_list)
+number = [int(x) for x in input()]
+evens_sum, odds_sum = odd_even_sum(number)
+print(f"Odd sum = {odds_sum}, Even sum = {evens_sum}")
+
+
+
+
+# def sum_all(numbers):
+#     even_sum = 0
+#     odd_sum = 0
+#     for num in numbers:
+#         if num % 2 == 0:
+#             even_sum += num
+#         else:
+#             odd_sum += num
+#     print(f"Odd sum = {odd_sum}, Even sum = {even_sum}")
+
+
+# numbers_input = input()
+# number_list = [int(digit) for digit in str(numbers_input)]
+# sum_all(number_list)
