96361f014f4e4b760868cc61f5f8f72920301cb3
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-25 21:05:52 +0200

    Update absolute_values.py

--- function_lab/absolute_values.py
+++ function_lab/absolute_values.py
@@ -1,8 +1,42 @@
-number_list = input().split()
+def take_input():
+    return list(map(float, input().split(" ")))
 
-absolute_value = []
 
-for num in number_list:
-    absolute_value.append(abs(float(num)))
+def take_abs_values(values):
+    return [abs(num) for num in values]
 
-print(absolute_value)
+
+def print_result(result):
+    return print(result)
+
+
+current_input = take_input()
+print_result(take_abs_values(current_input))
+
+
+
+
+##
+# def print_absolute_values():
+#     return print([abs(num) for num in list(map(float, input().split(" ")))])
+#
+#
+# print_absolute_values()
+
+
+
+
+##
+# print([abs(num) for num in list(map(float, input().split(" ")))])
+
+
+
+
+##
+# number_list = input().split()
+# absolute_value = []
+#
+# for num in number_list:
+#     absolute_value.append(abs(float(num)))
+#
+# print(absolute_value)
