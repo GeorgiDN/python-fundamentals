651a764f6a84d74f63c24bfbb0213bfe094ef03e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-25 21:09:36 +0200

    Update characters_in_range.py

--- function_exercise/characters_in_range.py
+++ function_exercise/characters_in_range.py
@@ -1,12 +1,22 @@
-def print_chars_between(start, end):
-    start_code = ord(start) + 1
-    end_code = ord(end)
+def characters_in_range(*args):
+    return print(*[chr(i) for i in range(ord(args[0]) + 1, ord(args[1]))])
 
-    for code in range(start_code, end_code):
-        char = chr(code)
-        print(char, end=" ")
 
-start_char = input()
-end_char = input()
+first = input()
+second = input()
 
-print_chars_between(start_char, end_char)
+characters_in_range(first, second)
+
+
+# def print_chars_between(start, end):
+#     start_code = ord(start) + 1
+#     end_code = ord(end)
+
+#     for code in range(start_code, end_code):
+#         char = chr(code)
+#         print(char, end=" ")
+
+# start_char = input()
+# end_char = input()
+
+# print_chars_between(start_char, end_char)
