cdf663c3423107c58b17131509486a5a5e5ec36e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-25 21:09:12 +0200

    Update add_and_subtract.py

--- function_exercise/add_and_subtract.py
+++ function_exercise/add_and_subtract.py
@@ -1,8 +1,10 @@
-def sum_numbers(a, b):
-    return a + b
+def sum_numbers(*args):
+    return sum(args)
 
-def subtract(a, b, c):
-    return sum_numbers(a, b) - c
+
+def subtract(*args):
+    return sum_numbers(args[0], args[1]) - args[2]
+
 
 def add_and_subtract():
     num_1 = int(input())
@@ -12,4 +14,23 @@ def add_and_subtract():
     result = subtract(num_1, num_2, num_3)
     print(result)
 
+
 add_and_subtract()
+
+
+
+# def sum_numbers(a, b):
+#     return a + b
+
+# def subtract(a, b, c):
+#     return sum_numbers(a, b) - c
+
+# def add_and_subtract():
+#     num_1 = int(input())
+#     num_2 = int(input())
+#     num_3 = int(input())
+
+#     result = subtract(num_1, num_2, num_3)
+#     print(result)
+
+# add_and_subtract()
