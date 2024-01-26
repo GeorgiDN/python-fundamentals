337b3052678aa3cecae6f005b8a93ec1cffd3de4
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-26 19:43:45 +0200

    Update double_char.py

--- basic_syntax_conditional_statements_and_loops_exercise/double_char.py
+++ basic_syntax_conditional_statements_and_loops_exercise/double_char.py
@@ -1,10 +1,34 @@
-command = input()
+class DoubleChar:
 
-while command != 'End':
-    if command == 'SoftUni':
-        command = input()
+    def __init__(self):
+        pass
 
-    for i in command:
-        print(i * 2, end='')
-    print()
-    command = input()
+    def implement_double_char(self):
+        command = self._take_input()
+
+        while command != 'End':
+            if command != 'SoftUni':
+                for i in command:
+                    print(i * 2, end='')
+                print()
+
+            command = self._take_input()
+
+    def _take_input(self):
+        return input()
+
+
+double_char = DoubleChar()
+double_char.implement_double_char()
+
+
+# command = input()
+
+# while command != 'End':
+#     if command == 'SoftUni':
+#         command = input()
+
+#     for i in command:
+#         print(i * 2, end='')
+#     print()
+#     command = input()
