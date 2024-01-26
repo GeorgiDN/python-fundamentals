31034779436cc3c84d0b2cf59285b2904a607025
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-26 20:08:45 +0200

    Update courses.py

--- lists_basics_lab/courses.py
+++ lists_basics_lab/courses.py
@@ -1,9 +1,18 @@
 lines = int(input())
-
-my_list = []
-
-for i in range(lines):
-    data = input()
-    my_list.append(data)
-
+my_list = [input() for _ in range(lines)]
 print(my_list)
+
+
+# lines = int(input())
+# print([input() for _ in range(lines)])
+
+# lines = int(input())
+#
+# my_list = []
+#
+# for i in range(lines):
+#     data = input()
+#     my_list.append(data)
+#
+# print(my_list)
+#
