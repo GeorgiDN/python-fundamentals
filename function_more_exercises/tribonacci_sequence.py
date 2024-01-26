d2f21d777569d069d1e297bd316371c293243795
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-26 21:00:05 +0200

    Update tribonacci_sequence.py

--- function_more_exercises/tribonacci_sequence.py
+++ function_more_exercises/tribonacci_sequence.py
@@ -1,18 +1,42 @@
-def tribonacci(n):
+def get_tribonacci_numbers(number):
+    first_num = 1
+    second_num = 1
+    third_num = 2
+    tribonacci_list = [first_num]
+    if number > 1:
+        tribonacci_list.append(second_num)
+    if number > 2:
+        tribonacci_list.append(third_num)
+    while len(tribonacci_list) < number:
+        for i in range(number - 3):
+            current_num = first_num + second_num + third_num
+            first_num = second_num
+            second_num = third_num
+            third_num = current_num
+            tribonacci_list.append(current_num)
+    return tribonacci_list
 
-    a = 0
-    b = 1
-    c = 1
 
-    print(b, c, end=' ')
+user_number = int(input())
+print(*get_tribonacci_numbers(user_number))
 
-    for i in range(2, n):
-        d = a + b + c
-        a = b
-        b = c
-        c = d
-        print(d, end=' ')
 
-num = int(input())
+# 90/100
+# def tribonacci(n):
 
-tribonacci(num)
+#     a = 0
+#     b = 1
+#     c = 1
+
+#     print(b, c, end=' ')
+
+#     for i in range(2, n):
+#         d = a + b + c
+#         a = b
+#         b = c
+#         c = d
+#         print(d, end=' ')
+
+# num = int(input())
+
+# tribonacci(num)
