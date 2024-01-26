0f9f245e0cdd93e8c7ac37035e18b8da4a4613de
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-26 19:54:21 +0200

    Update next_happy_year.py

--- data_types_and_variables_lab/next_happy_year.py
+++ data_types_and_variables_lab/next_happy_year.py
@@ -1,9 +1,27 @@
-year = int(input())
+def increase_year(year):
+    return year + 1
 
-while True:
-    year += 1
-    year_str = str(year)
-    if len(set(year_str)) == len(year_str):
-        break
 
-print(year)
+def find_next_happy_year(curr_year):
+    while True:
+        curr_year = increase_year(curr_year)
+        year_string = str(curr_year)
+        if len(set(year_string)) == len(year_string):
+            break
+
+    return curr_year
+
+
+current_year = int(input())
+print(find_next_happy_year(current_year))
+
+
+# year = int(input())
+
+# while True:
+#     year += 1
+#     year_str = str(year)
+#     if len(set(year_str)) == len(year_str):
+#         break
+
+# print(year)
