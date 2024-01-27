bfb362aa87f1906348be2e7d57f0e1c48ce80b5a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 15:07:54 +0200

    Update perfect_number.py

--- function_exercise/perfect_number.py
+++ function_exercise/perfect_number.py
@@ -1,14 +1,44 @@
+def perfect_number(num):
+    divisors = [divisor for divisor in range(1, (num // 2) + 1) if num % divisor == 0]
+    if sum(divisors) == num:
+        return "We have a perfect number!"
+    return "It's not so perfect."
 
-def perfect_number():
-    sum_of_divisors = 0
-    for i in range(1, number):
-        if number % i == 0:
-            sum_of_divisors += i
-
-    if sum_of_divisors == number:
-        print('We have a perfect number!')
-    else:
-        print("It's not so perfect.")
 
 number = int(input())
-perfect_number()
+print(perfect_number(number))
+
+
+
+
+# def perfect_number(num):
+#     divisors = []
+#     for divisor in range(1, (num // 2) + 1):
+#         if num % divisor == 0:
+#             divisors.append(divisor)
+# 
+#     if sum(divisors) == num:
+#         return "We have a perfect number!"
+#     return "It's not so perfect."
+# 
+# 
+# number = int(input())
+# print(perfect_number(number))
+
+
+
+
+# def perfect_number():
+#     sum_of_divisors = 0
+#     for i in range(1, number):
+#         if number % i == 0:
+#             sum_of_divisors += i
+#
+#     if sum_of_divisors == number:
+#         print('We have a perfect number!')
+#     else:
+#         print("It's not so perfect.")
+#
+#
+# number = int(input())
+# perfect_number()
