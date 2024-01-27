d57f997757cd9c886ba3f0b44f053e47a1c59986
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 21:45:59 +0200

    Update 5_office_chairs.py

--- lists_advanced_exercise/5_office_chairs.py
+++ lists_advanced_exercise/5_office_chairs.py
@@ -1,17 +1,41 @@
 rooms = int(input())
+game_on = True
 
-chairs_left = 0
-enough_chairs = True
-
+free_chairs = 0
 for room in range(1, rooms + 1):
-    info = input().split()
-    chairs = len(info[0])
-    visitor = int(info[1])
-    if chairs < visitor:
-        enough_chairs = False
-        print(f"{visitor - chairs} more chairs needed in room {room}")
-    else:
-        chairs_left += abs(chairs - visitor)
+    chairs, visitors = input().split(" ")
+    number_chairs = len(chairs)
+    number_visitors = int(visitors)
 
-if enough_chairs:
-    print(f"Game On, {chairs_left} free chairs left")
+    difference = abs(number_visitors - number_chairs)
+
+    if number_chairs > number_visitors:
+        free_chairs += difference
+
+    elif number_chairs < number_visitors:
+        game_on = False
+        print(f"{difference} more chairs needed in room {room}")
+
+if game_on:
+    print(f"Game On, {free_chairs} free chairs left")
+
+
+
+
+# rooms = int(input())
+
+# chairs_left = 0
+# enough_chairs = True
+
+# for room in range(1, rooms + 1):
+#     info = input().split()
+#     chairs = len(info[0])
+#     visitor = int(info[1])
+#     if chairs < visitor:
+#         enough_chairs = False
+#         print(f"{visitor - chairs} more chairs needed in room {room}")
+#     else:
+#         chairs_left += abs(chairs - visitor)
+
+# if enough_chairs:
+#     print(f"Game On, {chairs_left} free chairs left")
