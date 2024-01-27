9ee683a5dcd125d46e09680aa4a0a290e139b705
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 20:37:29 +0200

    Update 1_which_are_in.py

--- lists_advanced_exercise/1_which_are_in.py
+++ lists_advanced_exercise/1_which_are_in.py
@@ -1,12 +1,38 @@
-first_line = input().split(', ')
-second_line = input().split(', ')
+substrings_input = [x for x in input().split(", ")]
+words = [x for x in input().split(", ")]
 
-check_list = []
-
-for substring in first_line:
-    for word in second_line:
+substring_in_words = []
+for substring in substrings_input:
+    for word in words:
         if substring in word:
-            check_list.append(substring)
+            substring_in_words.append(substring)
             break
 
-print(check_list)
+print(substring_in_words)
+
+
+
+# substrings_input = [x for x in input().split(", ")]
+# words = [x for x in input().split(", ")]
+# substring_in_words = [substring for substring in substrings_input for word in words if substring in word]
+# checked_substrings = []
+#
+# for substring in substring_in_words:
+#     if substring not in checked_substrings:
+#         checked_substrings.append(substring)
+#
+# print(checked_substrings)
+
+
+# first_line = input().split(', ')
+# second_line = input().split(', ')
+
+# check_list = []
+
+# for substring in first_line:
+#     for word in second_line:
+#         if substring in word:
+#             check_list.append(substring)
+#             break
+
+# print(check_list)
