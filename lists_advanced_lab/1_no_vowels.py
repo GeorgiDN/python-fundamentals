a9948ff9205e47036be8c03b49f8cf9ebfcd774a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 17:09:21 +0200

    Update 1_no_vowels.py

--- lists_advanced_lab/1_no_vowels.py
+++ lists_advanced_lab/1_no_vowels.py
@@ -1,4 +1,26 @@
 text = input()
-sorted_text = [letter for letter in text if letter not in ['a', 'o', 'u', 'e', 'i', 'A', 'E', 'I', 'O', 'U']]
+print(''.join([letter for letter in text if letter.lower() not in ['a', 'o', 'u', 'e', "i"]]))
 
-print(''.join(sorted_text))
+
+# text = input()
+# sorted_text = [letter for letter in text if letter.lower() not in ['a', 'o', 'u', 'e', "i"]]
+# print(''.join(sorted_text))
+
+
+
+# text = input()
+# sorted_text = [letter for letter in text if letter not in ['a', 'o', 'u', 'e', 'i', 'A', 'E', 'I', 'O', 'U']]
+# print(''.join(sorted_text))
+
+
+
+# text = input()
+# vowels = ['a', 'o', 'u', 'e', 'i', 'A', 'E', 'I', 'O', 'U']
+#
+# letters_list = []
+#
+# for letter in text:
+#     if letter not in vowels:
+#           letters_list.append(letter)
+#
+# print(''.join(letters_list))
