3527a0315b816ebbe5bf567ee1fce5ab767c9072
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 21:05:41 +0200

    Update 2_next_version.py

--- lists_advanced_exercise/2_next_version.py
+++ lists_advanced_exercise/2_next_version.py
@@ -1,14 +1,29 @@
-version = list(map(int, input().split('.')))
-next_version = ""
+version = input().split(".")
+num1, num2, num3 = int(version[0]), int(version[1]), int(version[2])
 
-if version[2] < 9:
-    next_version = f'{version[0]}.{version[1]}.{version[2] + 1}'
-elif version[1] < 9:
-    next_version = f'{version[0]}.{version[1] + 1}.0'
-else:
-    next_version = f'{version[0] + 1}.0.0'
+num3 += 1
+if num3 >= 9:
+    num3 = 0
+    num2 += 1
+    if num2 == 10:
+        num2 = 0
+        num1 += 1
 
-print(next_version)
+print(f"{num1}.{num2}.{num3}")
+
+
+
+# version = list(map(int, input().split('.')))
+# next_version = ""
+
+# if version[2] < 9:
+#     next_version = f'{version[0]}.{version[1]}.{version[2] + 1}'
+# elif version[1] < 9:
+#     next_version = f'{version[0]}.{version[1] + 1}.0'
+# else:
+#     next_version = f'{version[0] + 1}.0.0'
+
+# print(next_version)
 
 
 
