31dc099ace9ed152b3f60855f694249e09ac3eb8
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 15:52:29 +0200

    Update factorial_division.py

--- function_exercise/factorial_division.py
+++ function_exercise/factorial_division.py
@@ -1,6 +1,7 @@
 def factorial_division(first_num, second_num):
     return round(factorial(first_num) / factorial(second_num), 2)
 
+
 def factorial(num):
     factorial_result = 1
     for i in range(1, num + 1):
@@ -8,9 +9,8 @@ def factorial(num):
     return factorial_result
 
 
-first_num = int(input())
-second_num = int(input())
+first_number = int(input())
+second_number = int(input())
 
-result = factorial_division(first_num, second_num)
+result = factorial_division(first_number, second_number)
 print(f"{result:.2f}")
-
