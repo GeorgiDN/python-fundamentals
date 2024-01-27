61883edf5db9bdb9303b91c3a7af31fcef4f74a7
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 18:14:13 +0200

    Update 4_palindrome_string.py

--- lists_advanced_lab/4_palindrome_string.py
+++ lists_advanced_lab/4_palindrome_string.py
@@ -1,35 +1,68 @@
-words = input().split(' ')
-searched_palindrome = input()
+words = input().split(" ")
+palindrome = input()
 
-palindromes = []
-count_palindromes = 0
+all_palindromes = [word for word in words if word == word[::-1]]
+number_of_searching_palindrome = len([word for word in words if word == palindrome])
 
-for i in words:
-    if i == i[::-1]:
-        palindromes.append(i)
-        if i == searched_palindrome:
-            count_palindromes += 1
+print(all_palindromes)
+print(f"Found palindrome {number_of_searching_palindrome} times")
 
 
-print(palindromes)
-print(f"Found palindrome {count_palindromes} times")
+# words, palindrome = input().split(" "), input()
+# print(f"{[word for word in words if word == word[::-1]]}\n"
+#       f"Found palindrome {len([word for word in words if word == palindrome])} times")
+
+
+
+# def palindrome(string, palindrome):
+#
+#     palindromes = [i for i in string if i == i[::-1]]
+#     palindrome_count = palindromes.count(palindrome)
+#     print(palindromes)
+#     print(f"Found palindrome {palindrome_count} times")
+#
+#
+# words = input().split(' ')
+# searched_palindrome = input()
+#
+# palindrome(words, searched_palindrome)
 
 
 
 # def palindrome(string, palindrome_count):
 #     palindromes = []
 #     palindrome_count = 0
-
+#
 #     for i in string:
 #         if i == i[::-1]:
 #             palindromes.append(i)
 #             if i == searched_palindrome:
 #                 palindrome_count += 1
-
+#
 #     print(palindromes)
 #     print(f"Found palindrome {palindrome_count} times")
+#
+#
+# words = input().split(' ')
+# searched_palindrome = input()
+#
+# palindrome(words, searched_palindrome)
+
+
+
 
 # words = input().split(' ')
 # searched_palindrome = input()
-
-# palindrome(words, searched_palindrome)
+#
+# palindromes = []
+# count_palindromes = 0
+#
+# for i in words:
+#     if i == i[::-1]:
+#         palindromes.append(i)
+#         if i == searched_palindrome:
+#             count_palindromes += 1
+#
+#
+# print(palindromes)
+# print(f"Found palindrome {count_palindromes} times")
