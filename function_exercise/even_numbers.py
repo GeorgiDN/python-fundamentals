f62f051598881bdcb2ddf6fa6ba3e96c50918800
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 15:52:53 +0200

    Update even_numbers.py

--- function_exercise/even_numbers.py
+++ function_exercise/even_numbers.py
@@ -1,11 +1,28 @@
-def list_even(list_with_numbers):
-    even_list = [num for num in list_with_numbers if num % 2 == 0]
-
-    # for num in list_with_numbers:
-    #     if num % 2 == 0:
-    #         even_list.append(num)
-    return print(even_list)
-
-
 number_list = list(map(int, input().split()))
-list_even(number_list)
+even_list = list(filter(lambda num: num % 2 == 0, number_list))
+print(even_list)
+
+
+##
+# def list_even(list_with_numbers):
+#     even_list = [num for num in list_with_numbers if num % 2 == 0]
+
+#     # for num in list_with_numbers:
+#     #     if num % 2 == 0:
+#     #         even_list.append(num)
+#     return print(even_list)
+
+##
+# number_list = list(map(int, input().split()))
+# list_even(number_list)
+
+
+##
+# number_list = [int(x) for x in input().split()]
+# even_list = [num for num in number_list if num % 2 == 0]
+# print(even_list)
+
+
+##
+# number_list = [int(x) for x in input().split()]
+# print([num for num in number_list if num % 2 == 0])
