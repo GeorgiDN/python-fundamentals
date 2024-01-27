0b99a66ef27c5059839ea148f176df4810c6bddd
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 20:09:19 +0200

    Update 7_the_office.py

--- lists_advanced_lab/7_the_office.py
+++ lists_advanced_lab/7_the_office.py
@@ -1,14 +1,30 @@
-employees = input().split(' ')
-list_employees = [int(i) for i in employees]
+list_of_employees = [int(x) for x in input().split(" ")]
 happiness_factor = int(input())
-improved_happiness = [num * happiness_factor for num in list_employees]
 
-average_happiness = sum(improved_happiness) / len(improved_happiness)
-happy_count = sum(h >= average_happiness for h in improved_happiness)
-total_count = len(improved_happiness)
-message = 'happy' if happy_count >= total_count / 2 else 'not happy'
+employees_multiplied_by_happiness_factor = [num * happiness_factor for num in list_of_employees]
+average_happiness_factor = sum(employees_multiplied_by_happiness_factor) / len(employees_multiplied_by_happiness_factor)
+number_happy_employees = len([num for num in employees_multiplied_by_happiness_factor if num >= average_happiness_factor])
+half_people = len(list_of_employees) / 2
 
-print(f'Score: {happy_count}/{total_count}. Employees are {message}!')
+if number_happy_employees >= half_people:
+    print(f"Score: {number_happy_employees}/{len(list_of_employees)}. Employees are happy!")
+else:
+    print(f"Score: {number_happy_employees}/{len(list_of_employees)}. Employees are not happy!")
+
+
+
+# employees = input().split(' ')
+# list_employees = [int(i) for i in employees]
+# happiness_factor = int(input())
+# improved_happiness = [num * happiness_factor for num in list_employees]
+# 
+# average_happiness = sum(improved_happiness) / len(improved_happiness)
+# happy_count = sum(h >= average_happiness for h in improved_happiness)
+# total_count = len(improved_happiness)
+# message = 'happy' if happy_count >= total_count / 2 else 'not happy'
+# 
+# print(f'Score: {happy_count}/{total_count}. Employees are {message}!')
+
 
 
 
