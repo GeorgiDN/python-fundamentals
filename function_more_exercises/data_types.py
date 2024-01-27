78b5e0bbd64ee0f847d5cf388e77618729c8f245
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 16:07:17 +0200

    Update data_types.py

--- function_more_exercises/data_types.py
+++ function_more_exercises/data_types.py
@@ -1,19 +1,34 @@
-def current_input(command, num):
-    result = None
-    if command == 'int':
-        num = int(num)
-        result = num * 2
-    elif command == 'real':
-        num = float(num)
-        result = num * 1.5
-        result = f'{result:.2f}'
-    elif command == 'string':
-        num = str(num)
-        result = f"${num}$"
+def data_types_implementation(kind, char):
+    if kind == "int":
+        return int(char) * 2
+    if kind == "real":
+        return f"{(float(char) * 1.5):.02f}"
+    if kind == "string":
+        return f"${char}$"
 
-    return result
 
-determiners = input()
-user_input = input()
+current_kind = input()
+current_string = input()
+print(data_types_implementation(current_kind, current_string))
 
-print(current_input(determiners, user_input))
+
+
+# def current_input(command, num):
+#     result = None
+#     if command == 'int':
+#         num = int(num)
+#         result = num * 2
+#     elif command == 'real':
+#         num = float(num)
+#         result = num * 1.5
+#         result = f'{result:.2f}'
+#     elif command == 'string':
+#         num = str(num)
+#         result = f"${num}$"
+
+#     return result
+
+# determiners = input()
+# user_input = input()
+
+# print(current_input(determiners, user_input))
