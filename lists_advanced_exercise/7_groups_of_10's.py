755a08897757c0d4fc1295f93be8bd274a91eddb
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 22:52:46 +0200

    Update  7_groups_of_10's.py

---
+++ lists_advanced_exercise/7_groups_of_10's.py
@@ -0,0 +1,38 @@
+list_numbers = [int(x) for x in input().split(", ")]
+groups = {}
+group = 0
+
+while True:
+    if len(list_numbers) == 0:
+        break
+
+    if max(list_numbers) > group:
+        group += 10
+
+    groups[group] = []
+
+    for i, num in enumerate(list_numbers):
+        if num <= group:
+            groups[group].append(num)
+
+    list_numbers = [x for x in list_numbers if x > group]
+
+for key, value in groups.items():
+    print(f"Group of {key}'s: {value}")
+
+
+
+# numbers_list = list(map(int, input().split(', ')))
+#
+# max_value_of_group = 10
+# groups = {}
+#
+# while numbers_list:
+#     numbers_according_group = [num for num in numbers_list if max_value_of_group - 10 < num <= max_value_of_group]
+#     groups[max_value_of_group] = numbers_according_group
+#     numbers_list = [num for num in numbers_list if num not in numbers_according_group]
+#
+#     max_value_of_group += 10
+#
+# for group, numbers in sorted(groups.items()):
+#     print(f"Group of {group}'s: {numbers}")
