76880f20a46d2ef5d8698eb57e07d1503e99e11e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-27 21:10:49 +0200

    Update 3_word_filter.py

--- lists_advanced_exercise/3_word_filter.py
+++ lists_advanced_exercise/3_word_filter.py
@@ -2,6 +2,14 @@ words = input().split(" ")
 print("\n".join([word for word in words if len(word) % 2 == 0]))
 
 
+
+# words = input().split(' ')
+# words_with_even_length = filter(lambda x: len(x) % 2 == 0, words)
+# for word in words_with_even_length:
+#     print(word)
+
+
+
 # words = input().split(' ')
 
 # for word in words:
