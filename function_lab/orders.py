25e9ca9521ac88c1a9ac85be694628272aa8eed1
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-31 19:55:50 +0200

    Update orders.py

--- function_lab/orders.py
+++ function_lab/orders.py
@@ -11,4 +11,9 @@ def total(product, number):
         result = 2.00 * number
     return f'{result:.2f}'
 
-print(total(product, number))
+
+current_product = input()
+current_number = int(input())
+
+
+print(total(current_product, current_number))
