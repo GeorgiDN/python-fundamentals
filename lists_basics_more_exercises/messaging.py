d4bb9ac50a64dfbe4270e4393c0ae386a21bdae0
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-01 20:31:42 +0200

    Update messaging.py

--- lists_basics_more_exercises/messaging.py
+++ lists_basics_more_exercises/messaging.py
@@ -1,13 +1,13 @@
 sequence = list(map(int, input().split()))
 text = input()
-
+ 
 message = ""
-print(len(text))
+ 
 for num in sequence:
     index = sum(map(int, str(num)))
     while index >= len(text):
         index -= len(text)
     message += text[index]
     text = text[:index] + text[index+1:]
-
+ 
 print(message)
