ec1babcbabf581b2faa2b71fd1212dbf5be2e1eb
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-01 22:05:50 +0200

    Update 4_zoo.py

--- object_and_casses/4_zoo.py
+++ object_and_casses/4_zoo.py
@@ -1,5 +1,6 @@
 class Zoo:
     __animals = 0
+
     def __init__(self, name):
         self.name = name
         self.mammals = []
@@ -7,36 +8,80 @@ class Zoo:
         self.birds = []
 
     def add_animals(self, species, name):
-        if species == 'mammal':
-            self.mammals.append(name)
-        elif species == 'fish':
-            self.fishes.append(name)
-        elif species == 'bird':
-            self.birds.append(name)
-
-        Zoo.__animals += 1
+        self._add_animal(species, name)
+        self.__animals += 1
 
     def get_info(self, species):
-        result = ""
-        if species == 'mammal':
+        return self._show_info(species)
+
+    # helper methods
+    def _add_animal(self, curr_species, curr_name):
+        if curr_species == "mammal":
+            self.mammals.append(curr_name)
+        elif curr_species == "fish":
+            self.fishes.append(curr_name)
+        elif curr_species == "bird":
+            self.birds.append(curr_name)
+
+    def _show_info(self, curr_species):
+        result = ''
+        if curr_species == 'mammal':
             result += f"Mammals in {self.name}: {', '.join(self.mammals)}\n"
-        elif species == 'fish':
+        elif curr_species == 'fish':
             result += f"Fishes in {self.name}: {', '.join(self.fishes)}\n"
-        elif species == 'bird':
+        elif curr_species == 'bird':
             result += f"Birds in {self.name}: {', '.join(self.birds)}\n"
 
-        result += f"Total animals: {Zoo.__animals}"
+        result += f"Total animals: {self.__animals}"
         return result
+
 
 zoo_name = input()
 zoo = Zoo(zoo_name)
 count = int(input())
 
-for i in range(count):
-    animal = input().split(" ")
-    species = animal[0]
-    name = animal[1]
-    zoo.add_animals(species, name)
 
-info = input()
-print(zoo.get_info((info)))
+
+
+# class Zoo:
+#     __animals = 0
+#     def __init__(self, name):
+#         self.name = name
+#         self.mammals = []
+#         self.fishes = []
+#         self.birds = []
+
+#     def add_animals(self, species, name):
+#         if species == 'mammal':
+#             self.mammals.append(name)
+#         elif species == 'fish':
+#             self.fishes.append(name)
+#         elif species == 'bird':
+#             self.birds.append(name)
+
+#         Zoo.__animals += 1
+
+#     def get_info(self, species):
+#         result = ""
+#         if species == 'mammal':
+#             result += f"Mammals in {self.name}: {', '.join(self.mammals)}\n"
+#         elif species == 'fish':
+#             result += f"Fishes in {self.name}: {', '.join(self.fishes)}\n"
+#         elif species == 'bird':
+#             result += f"Birds in {self.name}: {', '.join(self.birds)}\n"
+
+#         result += f"Total animals: {Zoo.__animals}"
+#         return result
+
+# zoo_name = input()
+# zoo = Zoo(zoo_name)
+# count = int(input())
+
+# for i in range(count):
+#     animal = input().split(" ")
+#     species = animal[0]
+#     name = animal[1]
+#     zoo.add_animals(species, name)
+
+# info = input()
+# print(zoo.get_info((info)))
