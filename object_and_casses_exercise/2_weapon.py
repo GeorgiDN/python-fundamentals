5db7d63316680fc2a8efc1e856b6d7897ff7e073
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-02 22:15:50 +0200

    Update 2_weapon.py

--- object_and_casses_exercise/2_weapon.py
+++ object_and_casses_exercise/2_weapon.py
@@ -1,16 +1,19 @@
 class Weapon:
+    __bullets = 0
+
     def __init__(self, bullets: int):
         self.bullets = bullets
+        Weapon.__bullets = self.bullets
 
     def shoot(self):
-        if self.bullets > 0:
-            self.bullets -= 1
-            return f"shooting..."
-        else:
-            return f"no bullets left"
+        if self.__bullets > 0:
+            self.__bullets -= 1
+            return "shooting..."
+        return "no bullets left"
 
     def __repr__(self):
-        return f"Remaining bullets: {self.bullets}"
+        return f"Remaining bullets: {self.__bullets}"
+
 
 weapon = Weapon(5)
 print(weapon.shoot())
@@ -21,3 +24,29 @@ print(weapon.shoot())
 print(weapon.shoot())
 print(weapon.shoot())
 print(weapon)
+
+
+
+# class Weapon:
+#     def __init__(self, bullets: int):
+#         self.bullets = bullets
+
+#     def shoot(self):
+#         if self.bullets > 0:
+#             self.bullets -= 1
+#             return f"shooting..."
+#         else:
+#             return f"no bullets left"
+
+#     def __repr__(self):
+#         return f"Remaining bullets: {self.bullets}"
+
+# weapon = Weapon(5)
+# print(weapon.shoot())
+# print(weapon.shoot())
+# print(weapon)
+# print(weapon.shoot())
+# print(weapon.shoot())
+# print(weapon.shoot())
+# print(weapon.shoot())
+# print(weapon)
