7db57fb5da1dd4e67497f31ca2196f58914f3e84
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-03 14:22:40 +0200

    Update 7_articles.py

--- object_and_casses_exercise/7_articles.py
+++ object_and_casses_exercise/7_articles.py
@@ -1,5 +1,4 @@
 class Article:
-
     def __init__(self, title: str, content: str, author: str):
         self.title = title
         self.content = content
@@ -17,10 +16,19 @@ class Article:
     def __repr__(self):
         return f"{self.title} - {self.content}: {self.author}"
 
-article = Article("Highest Recorded Temperature",
-"Temperatures across Europe are unprecedented, according to scientists.",
- "Ben Turner")
-article.edit("Syracuse, a city on the coast of the Italian island of Sicily, registered temperatures of 48.8 degrees Celsius")
-article.rename("Temperature in Italy")
-article.change_author("B. T.")
+
+article = Article(
+    "Highest Recorded Temperature",
+    "Temperatures across Europe are unprecedented, according to scientists.",
+    "Ben Turner"
+)
+article.edit(
+    "Syracuse, a city on the coast of the Italian island of Sicily, registered temperatures of 48.8 degrees Celsius"
+)
+article.rename(
+    "Temperature in Italy"
+)
+article.change_author(
+    "B. T."
+)
 print(article)
