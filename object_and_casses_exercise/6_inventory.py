4c3717333d5f24acd177e95ed737440c97c4a873
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-03 14:16:01 +0200

    Update 6_inventory.py

--- object_and_casses_exercise/6_inventory.py
+++ object_and_casses_exercise/6_inventory.py
@@ -1,26 +1,55 @@
 class Inventory:
-
-    def __init__(self, __capacity):
-        self.__capacity = __capacity
+    def __init__(self, capacity: int):
+        self.__capacity = capacity
         self.items = []
 
-    def add_item(self, item):
+    def add_item(self, item: str):
         if len(self.items) < self.__capacity:
             self.items.append(item)
-        else:
-            return "not enough room in the inventory"
+            return self.items
+        return "not enough room in the inventory"
 
     def get_capacity(self):
         return self.__capacity
 
     def __repr__(self):
-        left_capacity = self.__capacity-len(self.items)
-        return f"Items: {', '.join(self.items)}.\nCapacity left: {left_capacity}"
+        capacity_left = self.__capacity - len(self.items)
+        return f"Items: {', '.join(self.items)}.\nCapacity left: {capacity_left}"
 
 
 inventory = Inventory(2)
 inventory.add_item("potion")
 inventory.add_item("sword")
-inventory.add_item("bottle")
+print(inventory.add_item("bottle"))
 print(inventory.get_capacity())
 print(inventory)
+
+
+
+
+# class Inventory:
+
+#     def __init__(self, __capacity):
+#         self.__capacity = __capacity
+#         self.items = []
+
+#     def add_item(self, item):
+#         if len(self.items) < self.__capacity:
+#             self.items.append(item)
+#         else:
+#             return "not enough room in the inventory"
+
+#     def get_capacity(self):
+#         return self.__capacity
+
+#     def __repr__(self):
+#         left_capacity = self.__capacity-len(self.items)
+#         return f"Items: {', '.join(self.items)}.\nCapacity left: {left_capacity}"
+
+
+# inventory = Inventory(2)
+# inventory.add_item("potion")
+# inventory.add_item("sword")
+# inventory.add_item("bottle")
+# print(inventory.get_capacity())
+# print(inventory)
