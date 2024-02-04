ab8c9b9fc0ab405bd5c9b3543a09acd55db3351e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-04 17:23:21 +0200

    Update 6_odd_occurrences.py

--- dictionaries_lab/6_odd_occurrences.py
+++ dictionaries_lab/6_odd_occurrences.py
@@ -1,14 +1,13 @@
-words = input().split(" ")
-dictionary = {}
+words = input().split(' ')
 
+time_of_occurrences = {}
 for word in words:
     word_lower = word.lower()
-    if word_lower not in dictionary:
-        dictionary[word_lower] = 0
+    if word_lower not in time_of_occurrences:
+        time_of_occurrences[word_lower] = 0
+    time_of_occurrences[word_lower] += 1
 
-    dictionary[word_lower] += 1
-
-for (key, value) in dictionary.items():
-    if value % 2 != 0:
+for key, val in time_of_occurrences.items():
+    if val % 2 != 0:
         print(key, end=" ")
-      
+
