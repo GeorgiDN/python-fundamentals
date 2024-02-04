d680bc46e439f682abaa0f8845e0cb14d2d82b89
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-04 16:14:05 +0200

    Update 5_ascii_values.py

--- dictionaries_lab/5_ascii_values.py
+++ dictionaries_lab/5_ascii_values.py
@@ -1,9 +1,19 @@
-elements = input().split(", ")
-my_dict = {}
+letters = input().split(", ")
+print({letter: ord(letter) for letter in letters})
 
-for i in range(0, len(elements)):
-    key = elements[i]
-    value = ord(key)
-    my_dict[key] = int(value)
 
-print(my_dict)
+
+# characters = input().split(', ')
+# char_dict = {char: ord(char) for char in characters}
+# print(char_dict)
+
+
+# elements = input().split(", ")
+# my_dict = {}
+
+# for i in range(0, len(elements)):
+#     key = elements[i]
+#     value = ord(key)
+#     my_dict[key] = int(value)
+
+# print(my_dict)
