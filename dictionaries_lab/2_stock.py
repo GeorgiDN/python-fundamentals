9efc2e924e839d89036718ba13c80cb9ad94e0fe
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-04 14:52:08 +0200

    Update 2_stock.py

--- dictionaries_lab/2_stock.py
+++ dictionaries_lab/2_stock.py
@@ -1,14 +1,26 @@
-elements = input().split(" ")
-bakery = {}
+products_and_quantity = input().split(" ")
+products_with_quantity = {products_and_quantity[i]: int(products_and_quantity[i + 1])
+                          for i in range(0, len(products_and_quantity), 2)}
 
-for i in range(0, len(elements), 2):
-    key = elements[i]
-    value = elements[i + 1]
-    bakery[key] = int(value)
+check_products = input().split(" ")
 
-searched_products = input().split(" ")
-for product in searched_products:
-    if product in bakery:
-        print(f"We have {bakery[product]} of {product} left")
-    else:
-        print(f"Sorry, we don't have {product}")
+print('\n'.join([f"We have {products_with_quantity[product]} of {product} left" if product in products_with_quantity
+                 else f"Sorry, we don't have {product}" for product in check_products]))
+
+
+
+
+# elements = input().split(" ")
+# bakery = {}
+
+# for i in range(0, len(elements), 2):
+#     key = elements[i]
+#     value = elements[i + 1]
+#     bakery[key] = int(value)
+
+# searched_products = input().split(" ")
+# for product in searched_products:
+#     if product in bakery:
+#         print(f"We have {bakery[product]} of {product} left")
+#     else:
+#         print(f"Sorry, we don't have {product}")
