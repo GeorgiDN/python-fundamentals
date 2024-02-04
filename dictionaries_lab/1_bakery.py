5df39ca0f1e2e2e2f503d214aa3a9406d3b8fea9
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-04 14:36:53 +0200

    Update 1_bakery.py

--- dictionaries_lab/1_bakery.py
+++ dictionaries_lab/1_bakery.py
@@ -1,9 +1,17 @@
 elements = input().split(" ")
-keys = elements[::2]  
-values = elements[1::2]  
-bakery = dict(zip(keys, map(int, values)))
+bakery = {elements[i]: int(elements[i + 1]) for i in range(0, len(elements), 2)}
 
 print(bakery)
+
+
+
+
+# elements = input().split(" ")
+# keys = elements[::2]  
+# values = elements[1::2]  
+# bakery = dict(zip(keys, map(int, values)))
+
+# print(bakery)
 
 
 
