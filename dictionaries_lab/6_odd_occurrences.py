a28b562f04295bd45663f5d6124f4934b2acf278
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-04 17:34:22 +0200

    Update 6_odd_occurrences.py

--- dictionaries_lab/6_odd_occurrences.py
+++ dictionaries_lab/6_odd_occurrences.py
@@ -3,11 +3,21 @@ words = input().split(' ')
 time_of_occurrences = {}
 for word in words:
     word_lower = word.lower()
-    if word_lower not in time_of_occurrences:
-        time_of_occurrences[word_lower] = 0
-    time_of_occurrences[word_lower] += 1
+    time_of_occurrences[word_lower] = time_of_occurrences.get(word_lower, 0) + 1
 
-for key, val in time_of_occurrences.items():
-    if val % 2 != 0:
-        print(key, end=" ")
+print(' '.join([key for key, val in time_of_occurrences.items() if val % 2 != 0]))
+
+
+# words = input().split(' ')
+
+# time_of_occurrences = {}
+# for word in words:
+#     word_lower = word.lower()
+#     if word_lower not in time_of_occurrences:
+#         time_of_occurrences[word_lower] = 0
+#     time_of_occurrences[word_lower] += 1
+
+# for key, val in time_of_occurrences.items():
+#     if val % 2 != 0:
+#         print(key, end=" ")
 
