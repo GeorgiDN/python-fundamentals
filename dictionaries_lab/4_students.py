9ac94feb26312a0b2e07d91bb50141310141c9a1
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-04 16:02:34 +0200

    Update 4_students.py

--- dictionaries_lab/4_students.py
+++ dictionaries_lab/4_students.py
@@ -1,6 +1,4 @@
-name_with_id = {}
-name_with_course = {}
-
+students_info = []
 searched_course = ""
 
 while True:
@@ -11,14 +9,35 @@ while True:
 
     data = info.split(":")
     name, id_, course = data[0], int(data[1]), data[2]
-    if name not in name_with_id:
-        name_with_id[name] = id_
-    if name not in name_with_course:
-        name_with_course[name] = course
+    students_info.append({"name": name, "id": id_, "course": course})
 
-for name_, course_ in name_with_course.items():
-    if searched_course.startswith(course_[0:3]):
-        print(f"{name_} - {name_with_id[name_]}")
+for student in students_info:
+    if searched_course.startswith(student["course"][0:2]):
+        print(f"{student['name']} - {student['id']}")
+
+
+
+# name_with_id = {}
+# name_with_course = {}
+
+# searched_course = ""
+
+# while True:
+#     info = input()
+#     if ":" not in info:
+#         searched_course = info
+#         break
+
+#     data = info.split(":")
+#     name, id_, course = data[0], int(data[1]), data[2]
+#     if name not in name_with_id:
+#         name_with_id[name] = id_
+#     if name not in name_with_course:
+#         name_with_course[name] = course
+
+# for name_, course_ in name_with_course.items():
+#     if searched_course.startswith(course_[0:3]):
+#         print(f"{name_} - {name_with_id[name_]}")
 
 
 
