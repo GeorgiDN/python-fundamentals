9cfc5a54b5deda43003d0e7895be1a62fa35fd41
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-04 15:08:30 +0200

    Update 3_statistics.py

--- dictionaries_lab/3_statistics.py
+++ dictionaries_lab/3_statistics.py
@@ -1,20 +1,45 @@
-products = {}
+products_with_quantity = {}
 
-command = input()
+while True:
+    information = input()
+    if information == "statistics":
+        break
 
-while command != 'statistics':
+    data = information.split(": ")
+    product, quantity = data[0], int(data[1])
+    if product not in products_with_quantity:
+        products_with_quantity[product] = quantity
+    else:
+        products_with_quantity[product] += quantity
 
-    tokens = command.split(": ")
-    product = tokens[0]
-    quantity = int(tokens[1])
+total_products = len(products_with_quantity)
+total_quantity = sum([val for val in products_with_quantity.values()])
 
-    if product not in products:
-        products[product] = 0
-    products[product] += quantity
-    command = input()
 print(f"Products in stock:")
-for (product, quantity) in products.items():
-    print(f"- {product}: {quantity}")
+print('\n'.join([f"- {key}: {val_}" for key, val_ in products_with_quantity.items()]))
+print(f"Total Products: {total_products}\n"
+      f"Total Quantity: {total_quantity}")
 
-print(f"Total Products: {len(products.keys())}")
-print(f"Total Quantity: {sum(products.values())}")
+
+
+
+# products = {}
+
+# command = input()
+
+# while command != 'statistics':
+
+#     tokens = command.split(": ")
+#     product = tokens[0]
+#     quantity = int(tokens[1])
+
+#     if product not in products:
+#         products[product] = 0
+#     products[product] += quantity
+#     command = input()
+# print(f"Products in stock:")
+# for (product, quantity) in products.items():
+#     print(f"- {product}: {quantity}")
+
+# print(f"Total Products: {len(products.keys())}")
+# print(f"Total Quantity: {sum(products.values())}")
