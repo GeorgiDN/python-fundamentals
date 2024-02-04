5ce1f0b5670fe27abe27289550c6b8ad0051b93b
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-04 17:56:53 +0200

    Update 7_word_synonyms.py

--- dictionaries_lab/7_word_synonyms.py
+++ dictionaries_lab/7_word_synonyms.py
@@ -10,4 +10,39 @@ for i in range(n):
 
 for word in synonyms:
     print(f"{word} - {', '.join(synonyms[word])}")
-  
+    
+    
+    
+# words_with_synonyms = {}
+# rows = int(input())
+# 
+# for row in range(1, (rows * 2) + 1):
+#     if row % 2 != 0:
+#         word = input()
+#         if word not in words_with_synonyms:
+#             words_with_synonyms[word] = []
+#     else:
+#         synonym = input()
+#         words_with_synonyms[word].append(synonym)
+# 
+# 
+# for word_, synonyms in words_with_synonyms.items():
+#     print(f"{word_} - {', '.join(synonyms)}")
+
+
+
+# n = int(input())
+# synonyms = {}
+#
+# for _ in range(n):
+#     word = input()
+#     synonym = input()
+#
+#     if word in synonyms:
+#         synonyms[word].append(synonym)
+#     else:
+#         synonyms[word] = [synonym]
+#
+# for word, synonym_list in synonyms.items():
+#     synonyms_str = ', '.join(synonym_list)
+#     print(f'{word} - {synonyms_str}')
