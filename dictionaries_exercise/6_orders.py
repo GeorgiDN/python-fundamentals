7b5751830283fcd1c5da7fa475229d843697b19e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-05 22:03:37 +0200

    Update 6_orders.py

--- dictionaries_exercise/6_orders.py
+++ dictionaries_exercise/6_orders.py
@@ -1,23 +1,55 @@
-command = input()
-my_dictionary = {}
+def orders_task():
+    products_info = {}
+    while True:
+        command = input()
+        if command == "buy":
+            break
 
-while command != 'buy':
-    products = command.split(' ')
+        info = command.split()
 
-    product = products[0]
-    price = float(products[1])
-    quantity = int(products[2])
+        product, price, quantity = info[0], float(info[1]), int(info[2])
+        if product not in products_info:
+            products_info[product] = [price, quantity]
+        else:
+            if price != products_info[product][0]:
+                products_info[product][0] = price
+            products_info[product][1] += quantity
 
-    if product not in my_dictionary:
-        my_dictionary[product] = {'price': price, 'quantity': quantity}
-    else:
-        my_dictionary[product]['quantity'] += quantity
-        if my_dictionary[product]['price'] != price:
-            my_dictionary[product]['price'] = price
+    result = []
+    for name, data in products_info.items():
+        price_ = data[0]
+        qty = data[1]
+        total_price = price_ * qty
+        result.append(f"{name} -> {total_price:.2f}")
 
-    command = input()
+    return print('\n'.join(result))
 
-for product, data in my_dictionary.items():
-    total_price = data['price'] * data['quantity']
-    print(f"{product} -> {total_price:.2f}")
-  
+
+orders_task()
+
+
+
+
+# products_info = {}
+#
+# while True:
+#     command = input()
+#     if command == 'buy':
+#         break
+#     products = command.split(' ')
+#
+#     product = products[0]
+#     price = float(products[1])
+#     quantity = int(products[2])
+#
+#     if product not in products_info:
+#         products_info[product] = {'price': price, 'quantity': quantity}
+#     else:
+#         products_info[product]['quantity'] += quantity
+#         if products_info[product]['price'] != price:
+#             products_info[product]['price'] = price
+#
+#
+# for product, data in products_info.items():
+#     total_price = data['price'] * data['quantity']
+#     print(f"{product} -> {total_price:.2f}")
