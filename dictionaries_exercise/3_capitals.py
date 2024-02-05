f0f0f8bee654d62a2366993c048b1febf3afe650
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-05 19:53:38 +0200

    Update 3_capitals.py

--- dictionaries_exercise/3_capitals.py
+++ dictionaries_exercise/3_capitals.py
@@ -1,8 +1,21 @@
-countries = input().split(", ")
-capitals = input().split(", ")
+def find_capitals():
+    countries = input().split(", ")
+    capitals = input().split(", ")
 
-country_capital_dict = {countries[i]: capitals[i] for i in range(len(countries))}
+    counties_with_capitals = {countries[i]: capitals[i] for i in range(len(countries))}
+    result = '\n'.join([f"{country} -> {capital} " for country, capital in counties_with_capitals.items()])
+    return print(result)
 
-for country, capital in country_capital_dict.items():
-    print(f"{country} -> {capital}")
+
+find_capitals()
+
+
+
+# countries = input().split(", ")
+# capitals = input().split(", ")
+
+# country_capital_dict = {countries[i]: capitals[i] for i in range(len(countries))}
+
+# for country, capital in country_capital_dict.items():
+#     print(f"{country} -> {capital}")
   
