7f770cbc6f9058c6606a83f4c79358aa0d02a0b1
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-05 19:07:31 +0200

    Update 1_count_chars_in_a_string.py

--- dictionaries_exercise/1_count_chars_in_a_string.py
+++ dictionaries_exercise/1_count_chars_in_a_string.py
@@ -1,13 +1,48 @@
-text = input()
-character_count = {}
+def count_chars_in_string(chars_in_string):
+    text = input()
 
-for char in text:
-    if char != " ":
-        if char in character_count:
-            character_count[char] += 1
-        else:
-            character_count[char] = 1
+    for char in text:
+        if char.strip():
+            if char not in chars_in_string:
+                chars_in_string[char] = 0
+            chars_in_string[char] += 1
 
-for char, count in character_count.items():
-    print(f"{char} -> {count}")
+    return chars_in_string
+
+
+chars_in_text = {}
+count_chars_in_string(chars_in_text)
+
+print('\n'.join([f"{ch} -> {num}" for ch, num in chars_in_text.items()]))
+
+
+
+
+# chars_in_string = {}
+# text = input()
+#
+# for char in text:
+#     if char.strip():
+#         if char not in chars_in_string:
+#             chars_in_string[char] = 0
+#         chars_in_string[char] += 1
+#
+# print('\n'.join([f"{ch} -> {num}" for ch, num in chars_in_string.items()]))
+
+
+
+
+
+# text = input()
+# character_count = {}
+
+# for char in text:
+#     if char != " ":
+#         if char in character_count:
+#             character_count[char] += 1
+#         else:
+#             character_count[char] = 1
+
+# for char, count in character_count.items():
+#     print(f"{char} -> {count}")
   
