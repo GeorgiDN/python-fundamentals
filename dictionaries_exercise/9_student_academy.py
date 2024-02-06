6f8cafe5e83bfc46169b330c66d1843911d06655
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-06 20:11:56 +0200

    Update 9_student_academy.py

--- dictionaries_exercise/9_student_academy.py
+++ dictionaries_exercise/9_student_academy.py
@@ -1,17 +1,39 @@
-n = int(input())
+def student_academy():
+    student_grades = {}
+    rows = int(input())
+    for _ in range(rows):
+        name = input()
+        grade = float(input())
+        if name not in student_grades:
+            student_grades[name] = []
+        student_grades[name].append(grade)
 
-register_grades = {}
+    for name_, grades in student_grades.items():
+        average_grade = sum(grades) / len(grades)
+        if average_grade >= 4.50:
+            print(f"{name_} -> {average_grade:.2f}")
 
-for i in range(n):
-    name = input()
-    grade = float(input())
+    return student_grades
 
-    if name not in register_grades:
-        register_grades[name] = [grade]
-    else:
-        register_grades[name].append(grade)
 
-for name, grades in register_grades.items():
-    average_grade = sum(grades) / len(grades)
-    if average_grade >= 4.50:
-        print(f"{name} -> {average_grade:.2f}")
+student_academy()
+
+
+
+# n = int(input())
+
+# register_grades = {}
+
+# for i in range(n):
+#     name = input()
+#     grade = float(input())
+
+#     if name not in register_grades:
+#         register_grades[name] = [grade]
+#     else:
+#         register_grades[name].append(grade)
+
+# for name, grades in register_grades.items():
+#     average_grade = sum(grades) / len(grades)
+#     if average_grade >= 4.50:
+#         print(f"{name} -> {average_grade:.2f}")
