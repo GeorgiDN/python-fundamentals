958cf5bf5c562e4356f61abeb68591e489241f33
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-06 20:29:08 +0200

    Update 10_company_users.py

--- dictionaries_exercise/10_company_users.py
+++ dictionaries_exercise/10_company_users.py
@@ -1,21 +1,52 @@
-command = input()
-companies_users = {}
+def company_users():
+    companies_info = {}
 
-while command != 'End':
-    info = command.split(' -> ')
-    company = info[0]
-    employ_id = info[1]
+    while True:
+        command = input()
+        if command == "End":
+            break
 
-    if company not in companies_users:
-        companies_users[company] = [employ_id]
-    else:
-        if employ_id not in companies_users[company]:
-            companies_users[company].append(employ_id)
+        data = command.split(" -> ")
+        company_name, employ_id = data[0], data[1]
 
-    command = input()
+        if company_name not in companies_info:
+            companies_info[company_name] = []
 
-for company, employees in companies_users.items():
-    print(f"{company}")
-    for employ_id in employees:
-        print(f"-- {employ_id}")
+        if employ_id not in companies_info[company_name]:
+            companies_info[company_name].append(employ_id)
+
+    for company, id_data in companies_info.items():
+        print(f"{company}")
+        for id_ in id_data:
+            print(f"-- {id_}")
+
+    return companies_info
+
+
+company_users()
+
+
+
+
+
+# command = input()
+# companies_users = {}
+
+# while command != 'End':
+#     info = command.split(' -> ')
+#     company = info[0]
+#     employ_id = info[1]
+
+#     if company not in companies_users:
+#         companies_users[company] = [employ_id]
+#     else:
+#         if employ_id not in companies_users[company]:
+#             companies_users[company].append(employ_id)
+
+#     command = input()
+
+# for company, employees in companies_users.items():
+#     print(f"{company}")
+#     for employ_id in employees:
+#         print(f"-- {employ_id}")
       
