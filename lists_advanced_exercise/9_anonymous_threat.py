5ca9c4a0ebc37fd47850bceb22b6c90438c23152
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-09 20:06:29 +0200

    Update 9_anonymous_threat.py

--- lists_advanced_exercise/9_anonymous_threat.py
+++ lists_advanced_exercise/9_anonymous_threat.py
@@ -1,118 +1,132 @@
-def merge_element(elements, start, end):
-    merge_result = ""
-    for i in range(start, end + 1):
-        merge_result += elements[i]
-
-    return merge_result
-
-
-words = input().split()
-
-while True:
-    line = input()
-    if line == "3:1":
-        break
-
-    command, first_arg, second_arg = line.split()
-    if command == "merge":
-        start_index, end_index = int(first_arg), int(second_arg)
-
-        start_index = max(0, start_index)
-        end_index = min(len(words) - 1, end_index)
-
-        if start_index >= end_index:
-            continue
-
-        merged_element = merge_element(words, start_index, end_index)
-        remove_count_ops = end_index - start_index + 1
-        for _ in range(remove_count_ops):
-            words.pop(start_index)
-        words.insert(start_index, merged_element)
-
-    elif command == "divide":
-        el_idx = int(first_arg)
-        partitions = int(second_arg)
-
-        element = words[el_idx]
-        elements_parts = []
-        partition_size = len(element) // partitions
-
-        current_partition = ''
-        for idx in range((partitions - 1) * partition_size):
-            current_partition += element[idx]
-            if len(current_partition) == partition_size:
-                elements_parts.append(current_partition)
-                current_partition = ''
-
-        current_partition = ''
-        for idx_ in range((partitions - 1) * partition_size, len(element)):
-            current_partition += element[idx_]
-
-        elements_parts.append(current_partition)
-
-        words.pop(el_idx)
-
-        for index_ in range(len(elements_parts)):
-            words.insert(el_idx + index_, elements_parts[index_])
-
-print(" ".join(words))
+def merge(list_name, start_index, end_index):
+    if start_index < 0:
+        start_index = 0
+    list_with_result = list_name[:start_index]
+    merged_element = ""
+    index_in_range = True
+    if end_index >= len(list_name):
+        end_index = len(list_name) - 1
+        index_in_range = False
+    for index in range((end_index + 1) - start_index):
+        current_index = index + start_index
+        merged_element += list_name[current_index]
+    list_with_result.append(merged_element)
+    if index_in_range:
+        list_with_result += list_name[(end_index + 1):]
+    return list_with_result
 
 
+def divide(list_name, index, partitions):
+    result_list = list_name[:index]
+    string_to_divide = list_name[index]
+    current_substring = ""
+    last_substring = ""
+    letters_in_partition = len(string_to_divide) // partitions
+    
+    if len(string_to_divide) % partitions != 0:
+        slice_index = (partitions - 1) * letters_in_partition
+        last_substring = string_to_divide[slice_index:]
+        string_to_divide = string_to_divide[:slice_index]
+    for letter in string_to_divide:
+        current_substring += letter
+        if len(current_substring) == letters_in_partition:
+            result_list.append(current_substring)
+            current_substring = ""
+    if last_substring != "":
+        result_list.append(last_substring)
+    if (index + 1) < len(list_name):
+        result_list += list_name[index + 1:]
+    return result_list
 
 
-#
-# def merge(list_name, start_index, end_index):
-#     if start_index < 0:
-#         start_index = 0
-#     result_list = list_name[:start_index]
-#     merged_element = ""
-#     index_is_in_range = True
-#     if end_index >= len(list_name):
-#         end_index = len(list_name) - 1
-#         index_is_in_range = False
-#     for index in range((end_index + 1) - start_index):
-#         current_index = index + start_index
-#         merged_element += list_name[current_index]
-#     result_list.append(merged_element)
-#     if index_is_in_range:
-#         result_list += list_name[(end_index + 1):]
-#     return result_list
-#
-#
-# def divide(list_name, index, partitions):
-#     result_list = list_name[:index]
-#     string_to_divide = list_name[index]
-#     current_substring = ""
-#     last_substring = ""
-#     letters_in_partition = len(string_to_divide) // partitions
-#     if len(string_to_divide) % partitions != 0:
-#         slice_index = (partitions - 1) * letters_in_partition
-#         last_substring = string_to_divide[slice_index:]
-#         string_to_divide = string_to_divide[:slice_index]
-#     for letter in string_to_divide:
-#         current_substring += letter
-#         if len(current_substring) == letters_in_partition:
-#             result_list.append(current_substring)
-#             current_substring = ""
-#     if last_substring != "":
-#         result_list.append(last_substring)
-#     if (index + 1) < len(list_name):
-#         result_list += list_name[index + 1:]
-#     return result_list
-#
-#
-# data_array = input().split()
-# command = input()
-# while command != "3:1":
-#     current_command = command.split()[0]
-#     first_value = int(command.split()[1])
-#     second_value = int(command.split()[2])
-#     if current_command == "merge":
-#         data_array = merge(data_array, first_value, second_value)
-#     elif current_command == "divide":
-#         data_array = divide(data_array, first_value, second_value)
-#     command = input()
-# print(" ".join(data_array))
+def anonymous_threat():
+    data_array = input().split()
+    while True:
+        command = input()
+        if command == "3:1":
+            break
+        info = command.split()
+        
+        current_command = info[0]
+        first_value = int(info[1])
+        second_value = int(info[2])
+        if current_command == "merge":
+            data_array = merge(data_array, first_value, second_value)
+        elif current_command == "divide":
+            data_array = divide(data_array, first_value, second_value)
+
+    print(" ".join(data_array))
+
+
+anonymous_threat()
+
+
+
+
+
+# def merge_element(elements, start, end):
+#     merge_result = ""
+#     for i in range(start, end + 1):
+#         merge_result += elements[i]
+
+#     return merge_result
+
+
+# words = input().split()
+
+# while True:
+#     line = input()
+#     if line == "3:1":
+#         break
+
+#     command, first_arg, second_arg = line.split()
+#     if command == "merge":
+#         start_index, end_index = int(first_arg), int(second_arg)
+
+#         start_index = max(0, start_index)
+#         end_index = min(len(words) - 1, end_index)
+
+#         if start_index >= end_index:
+#             continue
+
+#         merged_element = merge_element(words, start_index, end_index)
+#         remove_count_ops = end_index - start_index + 1
+#         for _ in range(remove_count_ops):
+#             words.pop(start_index)
+#         words.insert(start_index, merged_element)
+
+#     elif command == "divide":
+#         el_idx = int(first_arg)
+#         partitions = int(second_arg)
+
+#         element = words[el_idx]
+#         elements_parts = []
+#         partition_size = len(element) // partitions
+
+#         current_partition = ''
+#         for idx in range((partitions - 1) * partition_size):
+#             current_partition += element[idx]
+#             if len(current_partition) == partition_size:
+#                 elements_parts.append(current_partition)
+#                 current_partition = ''
+
+#         current_partition = ''
+#         for idx_ in range((partitions - 1) * partition_size, len(element)):
+#             current_partition += element[idx_]
+
+#         elements_parts.append(current_partition)
+
+#         words.pop(el_idx)
+
+#         for index_ in range(len(elements_parts)):
+#             words.insert(el_idx + index_, elements_parts[index_])
+
+# print(" ".join(words))
+
+
+
+
+
 
 
 
