1df676fb33766606e4dded78dcf4e2adc5235be3
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-11 12:06:09 +0200

    Update 2_repeat_string.py

--- text_processing_lab/2_repeat_string.py
+++ text_processing_lab/2_repeat_string.py
@@ -1,8 +1,30 @@
-strings = input().split(' ')
-result = ''
-for word in strings:
-    result += word * len(word)
-print(result)
+text_sequence = input().split()
+print(''.join([word * len(word) for word in text_sequence]))
+
+
+# def repeat_string():
+#     text_sequence = input().split()
+#     final_text = ''
+#     for word in text_sequence:
+#         len_word = len(word)
+#         repeated_word = word * len_word
+#         final_text += repeated_word
+#
+#     print(final_text)
+#
+#
+# repeat_string()
+
+
+
+
+# strings = input().split(' ')
+# result = ''
+# for word in strings:
+#     result += word * len(word)
+# print(result)
+
+
 
 
 # data = input().split()
