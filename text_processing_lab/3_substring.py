b731a13b3c19e2b3c1b065e9a32d628f533159df
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-11 12:14:20 +0200

    Update 3_substring.py

--- text_processing_lab/3_substring.py
+++ text_processing_lab/3_substring.py
@@ -1,7 +1,18 @@
-first_string = input()
-second_string = input()
+def substring_task():
+    curr_substring = input()
+    current_string = input()
+    while curr_substring in current_string:
+        current_string = current_string.replace(curr_substring, '')
+    print(current_string)
 
-while first_string in second_string:
-    second_string = second_string.replace(first_string, '')
 
-print(second_string)
+substring_task()
+
+
+# first_string = input()
+# second_string = input()
+
+# while first_string in second_string:
+#     second_string = second_string.replace(first_string, '')
+
+# print(second_string)
