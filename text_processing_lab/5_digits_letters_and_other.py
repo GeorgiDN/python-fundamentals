934023f449034aeb83289f6a4c1e2ed05596ec48
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-11 13:08:49 +0200

    Update 5_digits_letters_and_other.py

--- text_processing_lab/5_digits_letters_and_other.py
+++ text_processing_lab/5_digits_letters_and_other.py
@@ -1,18 +1,59 @@
-text = input()
-text_list = list(text)
+def digits_letters_and_other():
+    text = input()
 
-digits = ''
-letters = ''
-others = ''
+    digits = [char for char in text if char.isdigit()]
+    letters = [char for char in text if char.isalpha()]
+    other = [char for char in text if not char.isalpha() and not char.isdigit()]
 
-for i in text_list:
-    if i.isdigit():
-        digits += i
-    elif i.isalpha():
-        letters += i
-    else:
-        others += i
+    print(''.join(digits))
+    print(''.join(letters))
+    print(''.join(other))
 
-print(digits)
-print(letters)
-print(others)
+
+digits_letters_and_other()
+
+
+
+# def digits_letters_and_other():
+#     text = input()
+#     digits = ""
+#     letters = ""
+#     other = ""
+# 
+#     for char in text:
+#         if char.isdigit():
+#             digits += char
+#         elif char.isalpha():
+#             letters += char
+#         else:
+#             other += char
+# 
+#     print(digits)
+#     print(letters)
+#     print(other)
+# 
+# 
+# digits_letters_and_other()
+
+
+
+
+
+# text = input()
+# text_list = list(text)
+
+# digits = ''
+# letters = ''
+# others = ''
+
+# for i in text_list:
+#     if i.isdigit():
+#         digits += i
+#     elif i.isalpha():
+#         letters += i
+#     else:
+#         others += i
+
+# print(digits)
+# print(letters)
+# print(others)
