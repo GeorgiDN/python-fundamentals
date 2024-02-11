4bf4dd0c27cffb3a2893c8bd105e570307449b25
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-11 16:43:39 +0200

    Update 2_characters_multiplier.py

--- text_processing_exercise/2_characters_multiplier.py
+++ text_processing_exercise/2_characters_multiplier.py
@@ -1,17 +1,31 @@
-str1, str2 = input().split()
-
+string_1, string_2 = input().split()
 total_sum = 0
+min_length = min(len(string_1), len(string_2))
 
-min_length = min(len(str1), len(str2))
+total_sum += sum([ord(string_1[i]) * ord(string_2[i]) for i in range(min_length)])
 
-for i in range(min_length):
-    total_sum += ord(str1[i]) * ord(str2[i])
-
-if len(str1) > len(str2):
-    for i in range(min_length, len(str1)):
-        total_sum += ord(str1[i])
+if len(string_1) > len(string_2):
+    total_sum += sum([ord(string_1[i]) for i in range(min_length, len(string_1))])
 else:
-    for i in range(min_length, len(str2)):
-        total_sum += ord(str2[i])
+    total_sum += sum([ord(string_2[i]) for i in range(min_length, len(string_2))])
 
 print(total_sum)
+
+
+
+
+# string_1, string_2 = input().split()
+# total_sum = 0
+# min_length = min(len(string_1), len(string_2))
+#
+# for i in range(min_length):
+#     total_sum += ord(string_1[i]) * ord(string_2[i])
+#
+# if len(string_1) > len(string_2):
+#     for i in range(min_length, len(string_1)):
+#         total_sum += ord(string_1[i])
+# else:
+#     for i in range(min_length, len(string_2)):
+#         total_sum += ord(string_2[i])
+#
+# print(total_sum)
