838e82a863c467c430a70520527e76a8a095da8e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-12 21:06:11 +0200

    Update 1_computer_store.py

--- mid_exam_preparation/1_programming_fundamentals_mid_exam_retake/1_computer_store.py
+++ mid_exam_preparation/1_programming_fundamentals_mid_exam_retake/1_computer_store.py
@@ -1,41 +1,112 @@
-price_without_task = 0
-while True
-    command = input()
+def is_price_positive_number(tax_):
+    return tax_ >= 0
 
-    if command == 'special' or command == 'regular'
-        break
 
-    price = float(command)
-    if price  0
-        print('Invalid price!')
-        continue
+def increase_taxes_with_twenty_percent(prices_without_taxes_, prices_with_taxes_):
+    prices_with_taxes_ = [price * 1.20 for price in prices_without_taxes_]
+    return prices_with_taxes_
 
-    price_without_task += price
 
-taxes = price_without_task  0.2
-final_price = price_without_task + taxes
+def take_ten_percent_of_discount(total_sum_, prices_with_taxes_):
+    total_sum_ = total_sum_ * 0.9
+    return total_sum_
 
-if command == 'special'
-    if price_without_task  0
-        discount = final_price  0.1
-        final_price = final_price - discount
-        print(fCongratulations you've just bought a new computer!n
-              fPrice without taxes {price_without_task.2f}$n
-              fTaxes {taxes.2f}$n
-              f-----------n
-              fTotal price {final_price.2f}$)
-    else
-        print('Invalid order!')
 
-elif command == 'regular'
-    if price_without_task  0
-        print(fCongratulations you've just bought a new computer!n
-              fPrice without taxes {price_without_task.2f}$n
-              fTaxes {taxes.2f}$n
-              f-----------n
-              fTotal price {final_price.2f}$)
-    else
-        print('Invalid order!')
+def take_taxes(taxes_, prices_without_taxes_, prices_with_taxes_):
+    taxes_ = sum(prices_with_taxes_) - sum(prices_without_taxes_)
+    return taxes_
+    pass
+
+
+def print_result(total_price_without_taxes_, taxes_, total_sum_):
+    return print("Congratulations you've just bought a new computer!\n"
+                 f"Price without taxes: {total_price_without_taxes_:.2f}$\n"
+                 f"Taxes: {taxes_:.2f}$\n"
+                 "-----------\n"
+                 f"Total price: {total_sum_:.2f}$")
+
+
+def computer_store():
+    prices_without_taxes = []
+    prices_with_taxes = []
+    taxes = 0
+    type_of_customer = ""
+
+    while True:
+        command = input()
+        if command == "special":
+            type_of_customer = "special"
+            break
+        elif command == "regular":
+            type_of_customer = "regular"
+            break
+        current_tax = float(command)
+
+        if not is_price_positive_number(current_tax):
+            print("Invalid price!")
+            continue
+
+        prices_without_taxes.append(current_tax)
+
+    if prices_without_taxes:
+        total_price_without_taxes = sum(prices_without_taxes)
+        prices_with_taxes = increase_taxes_with_twenty_percent(prices_without_taxes, prices_with_taxes)
+        taxes = take_taxes(taxes, prices_without_taxes, prices_with_taxes)
+        total_sum = sum(prices_with_taxes)
+        if type_of_customer == "special":
+            total_sum = take_ten_percent_of_discount(total_sum, prices_with_taxes)
+
+        print_result(total_price_without_taxes, taxes, total_sum)
+
+    else:
+        print("Invalid order!")
+
+
+computer_store()
+
+
+
+
+
+
+# price_without_task = 0
+# while True
+#     command = input()
+
+#     if command == 'special' or command == 'regular'
+#         break
+
+#     price = float(command)
+#     if price  0
+#         print('Invalid price!')
+#         continue
+
+#     price_without_task += price
+
+# taxes = price_without_task  0.2
+# final_price = price_without_task + taxes
+
+# if command == 'special'
+#     if price_without_task  0
+#         discount = final_price  0.1
+#         final_price = final_price - discount
+#         print(fCongratulations you've just bought a new computer!n
+#               fPrice without taxes {price_without_task.2f}$n
+#               fTaxes {taxes.2f}$n
+#               f-----------n
+#               fTotal price {final_price.2f}$)
+#     else
+#         print('Invalid order!')
+
+# elif command == 'regular'
+#     if price_without_task  0
+#         print(fCongratulations you've just bought a new computer!n
+#               fPrice without taxes {price_without_task.2f}$n
+#               fTaxes {taxes.2f}$n
+#               f-----------n
+#               fTotal price {final_price.2f}$)
+#     else
+#         print('Invalid order!')
 
 
 
