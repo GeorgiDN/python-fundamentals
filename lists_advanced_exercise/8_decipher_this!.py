4eb4eb5a8cf0cab9d60e50a4bcf018d3d257bc89
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-13 22:20:45 +0200

    Update 8_decipher_this!.py

--- lists_advanced_exercise/8_decipher_this!.py
+++ lists_advanced_exercise/8_decipher_this!.py
@@ -29,6 +29,45 @@ print(''.join(final_decipher_string))
 
 
 
+
+# import re
+
+
+# string_input = input().split(" ")
+
+# decipher_string_1 = []
+# final_decipher_string = []
+
+# pattern = r"\d+"
+
+# number = ''
+# for word in string_input:
+#     for char in word:
+#         if re.match(pattern, char):
+#             number += char
+#         else:
+#             decipher_string_1.append(char)
+
+#     # find letter on the current number
+#     letter = chr(int(number))
+#     # insert letter in the beginning of string
+#     decipher_string_1.insert(0, letter)
+#     number = ''
+#     # swap second and last letter
+#     sec_letter = decipher_string_1[1]
+#     last_letter = decipher_string_1[-1]
+#     decipher_string_1[1] = last_letter
+#     decipher_string_1[-1] = sec_letter
+#     final_decipher_string.extend(decipher_string_1)
+#     final_decipher_string.extend(' ')
+#     decipher_string_1 = []
+
+# print(''.join(final_decipher_string))
+
+
+
+
+
 # my_list = input().split()
 # my_number_list = []
 # number_part = ''
