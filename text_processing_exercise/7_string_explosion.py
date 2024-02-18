e6bde4574fe22e392c2e00a45248075c6552a682
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-18 15:03:05 +0200

    Update 7_string_explosion.py

--- text_processing_exercise/7_string_explosion.py
+++ text_processing_exercise/7_string_explosion.py
@@ -1,17 +1,35 @@
-text = input()
-compressed_text = []
+text = list(input())
+final_text = text[:2]
+
 strength = 0
-i = 0
 
-while i < len(text):
-    compressed_text.append(text[i])
-    if text[i].isdigit():
-        strength += int(text[i])
+for idx in range(2, len(text)):
+    char = text[idx]
+    final_text.append(char)
+    if final_text[-2] == ">" and final_text[-1] != ">":
+        if char.isdigit():
+            strength += int(char)
+        if strength > 0:
+            final_text.pop()
+            strength -= 1
 
-    if strength > 0 and text[i] != '>':
-        compressed_text.pop()
-        strength -= 1
+print(''.join(final_text))
 
-    i += 1
 
-print(''.join(compressed_text))
+# text = input()
+# compressed_text = []
+# strength = 0
+# i = 0
+
+# while i < len(text):
+#     compressed_text.append(text[i])
+#     if text[i].isdigit():
+#         strength += int(text[i])
+
+#     if strength > 0 and text[i] != '>':
+#         compressed_text.pop()
+#         strength -= 1
+
+#     i += 1
+
+# print(''.join(compressed_text))
