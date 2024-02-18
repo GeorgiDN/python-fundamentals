530db7b667d9a5a4fac91d1e58354de24172c371
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-18 14:27:40 +0200

    Update 6_replace_repeating_chars.py

--- text_processing_exercise/6_replace_repeating_chars.py
+++ text_processing_exercise/6_replace_repeating_chars.py
@@ -1,11 +1,36 @@
-text_list = list(text)
+text = input()
+final_string = text[0]
 
-check_symbols = []
+for i in range(1, len(text)):
+    if text[i] != text[i - 1]:
+        final_string += text[i]
 
-for ch in text_list:
-    if ch not in check_symbols:
-        check_symbols.append(ch)
-    if check_symbols[-1] != ch:
-        check_symbols.append(ch)
+print(final_string)
 
-print(''.join(check_symbols))
+
+
+# string_input = input()
+# final_string = ""
+# 
+# for i in range(len(string_input)):
+#     char = string_input[i]
+#     if not final_string:
+#         final_string += char
+#     elif final_string[-1] != char:
+#         final_string += char
+# 
+# print(final_string)
+
+
+# text = input()
+# text_list = list(text)
+# 
+# check_symbols = []
+# 
+# for ch in text_list:
+#     if ch not in check_symbols:
+#         check_symbols.append(ch)
+#     if check_symbols[-1] != ch:
+#         check_symbols.append(ch)
+# 
+# print(''.join(check_symbols))
