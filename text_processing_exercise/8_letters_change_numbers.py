f6c295cf2053ba06654232c0d7e2286a22eced57
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-18 15:58:24 +0200

    Update 8_letters_change_numbers.py

--- text_processing_exercise/8_letters_change_numbers.py
+++ text_processing_exercise/8_letters_change_numbers.py
@@ -1,17 +1,51 @@
-text = input().split()
-final_result = 0
-for element in text:
-    first_letter = element[0]
-    last_letter = element[-1]
-    number = int(element[1:-1])
-    first_letter_position_in_alphabet = ord(first_letter.upper()) - 64
-    last_letter_position_in_alphabet = ord(last_letter.upper()) - 64
-    if first_letter.isupper():
-        final_result += number / first_letter_position_in_alphabet
-    else:
-        final_result += number * first_letter_position_in_alphabet
-    if last_letter.isupper():
-        final_result -= last_letter_position_in_alphabet
-    else:
-        final_result += last_letter_position_in_alphabet
-print(f"{final_result:.2f}")
+def find_letter_position_in_alphabet_starting_from_one(letter):
+    letter = ord(letter.lower()) - 96
+    return letter
+
+
+def main():
+    text = input().split()
+    result = 0
+    
+    for element in text:
+        first_letter = element[0]
+        last_letter = element[-1]
+        number = int(element[1:-1])
+        first_letter_position = find_letter_position_in_alphabet_starting_from_one(first_letter)
+        last_letter_position = find_letter_position_in_alphabet_starting_from_one(last_letter)
+        if first_letter.isupper():
+            result += number / first_letter_position
+        else:
+            result += number * first_letter_position
+        if last_letter.isupper():
+            result -= last_letter_position
+        else:
+            result += last_letter_position
+    
+    print(f"{result:.2f}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+#
+# text = input().split()
+# result = 0
+
+# for element in text:
+#     first_letter = element[0]
+#     last_letter = element[-1]
+#     number = int(element[1:-1])
+#     first_letter_position = ord(first_letter.lower()) - 96
+#     last_letter_position = ord(last_letter.lower()) - 96
+#     if first_letter.isupper():
+#         result += number / first_letter_position
+#     else:
+#         result += number * first_letter_position
+#     if last_letter.isupper():
+#         result -= last_letter_position
+#     else:
+#         result += last_letter_position
+
+# print(f"{result:.2f}")
