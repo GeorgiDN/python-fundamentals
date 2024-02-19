60911ec5ef95c9bf1583ad2da5d19402c0e724ea
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-19 19:53:31 +0200

    Update 2_ascii_sumator.py

--- text_processing_more_exercises/2_ascii_sumator.py
+++ text_processing_more_exercises/2_ascii_sumator.py
@@ -1,14 +1,30 @@
-first_char = input()
-sec_char = input()
-string = input()
-
-start_char = ord(first_char)
-end_char = ord(sec_char)
-
+start_character = input()
+end_character = input()
+text = input()
 total_sum = 0
 
-for ch in string:
-    if start_char < ord(ch) < end_char:
-        total_sum += ord(ch)
+range_start = ord(start_character)
+range_end = ord(end_character)
+
+for char in text:
+    if ord(char) in range(range_start + 1, range_end):
+        total_sum += ord(char)
 
 print(total_sum)
+
+
+
+# first_char = input()
+# sec_char = input()
+# string = input()
+
+# start_char = ord(first_char)
+# end_char = ord(sec_char)
+
+# total_sum = 0
+
+# for ch in string:
+#     if start_char < ord(ch) < end_char:
+#         total_sum += ord(ch)
+
+# print(total_sum)
