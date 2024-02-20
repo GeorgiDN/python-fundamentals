d81c3beca916f4022d7e467527b08e3cc2406b86
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-20 20:50:26 +0200

    Update 1_capture_the_numbers.py

--- regular_expressions_exercise/1_capture_the_numbers.py
+++ regular_expressions_exercise/1_capture_the_numbers.py
@@ -1,12 +1,34 @@
 import re
 
-numbers = []
 
+def extract_numbers(text_):
+    matches = re.findall(r"\d+", text_)
+    if matches:
+        return matches
+
+
+extracted_numbers = []
 while True:
-    txt = input()
-    if txt == "":
+    text = input()
+    if text == "":
         break
 
-    numbers.extend(re.findall(r'\d+', txt))
+    found_number = extract_numbers(text)
+    if found_number:
+        extracted_numbers.extend(found_number)
 
-print(' '.join(numbers))
+print(*extracted_numbers)
+
+
+# import re
+
+# numbers = []
+
+# while True:
+#     txt = input()
+#     if txt == "":
+#         break
+
+#     numbers.extend(re.findall(r'\d+', txt))
+
+# print(' '.join(numbers))
