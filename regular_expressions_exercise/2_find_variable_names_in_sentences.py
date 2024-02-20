5cb4175fba989e2ef30786bd93293beb9995cd68
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-20 21:19:43 +0200

    Update 2_find_variable_names_in_sentences.py

--- regular_expressions_exercise/2_find_variable_names_in_sentences.py
+++ regular_expressions_exercise/2_find_variable_names_in_sentences.py
@@ -1,29 +1,55 @@
 import re
 
 text = input()
-final_lst = []
-regex = r'\b_[A-Za-z0-9]+\b'
-matches = re.findall(regex, text)
+pattern = r'\b_[A-Za-z0-9]+\b'
+matches = re.findall(pattern, text)
 
-for word in matches:
-    new_word = word.replace('_', '')
-    final_lst.append(new_word)
-
-print(','.join(final_lst))
+found_variable = [re.sub("_", "", match) for match in matches]
+print(','.join(found_variable))
 
 
 
-
+########################################################################################
 # import re
+# print(','.join([re.sub("_", "", match) for match in re.findall(r'\b_[A-Za-z0-9]+\b', input())]))
 
-# txt = input()
-# regex = r'\b_[A-Za-z\d]*\b'
-# matches = re.findall(regex, txt)
 
-# result = ",".join(matches)
-# final_result = []
-# for i in result:
-#     if i != '_':
-#         final_result.append(i)
 
-# print(''.join(final_result))
+#####################################################################################
+# import re
+#
+# text = input()
+# matches = re.findall(r'\b_[A-Za-z0-9]+\b', text)
+# print(','.join([re.sub("_", "", match) for match in matches]))
+
+
+
+#####################################################################################
+# import re
+#
+# found_variable = []
+# text = input()
+# pattern = r'\b_[A-Za-z0-9]+\b'
+# matches = re.findall(pattern, text)
+#
+# for match in matches:
+#     variable_name = re.sub("_", "", match)
+#     found_variable.append(variable_name)
+#
+# print(','.join(found_variable))
+
+
+
+#####################################################################################
+# import re
+#
+# text = input()
+# final_lst = []
+# regex = r'\b_[A-Za-z0-9]+\b'
+# matches = re.findall(regex, text)
+#
+# for word in matches:
+#     new_word = word.replace('_', '')
+#     final_lst.append(new_word)
+#
+# print(','.join(final_lst))
