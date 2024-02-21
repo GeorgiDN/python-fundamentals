30fc869f67812ce2e7fe130e0a19d3f893734b17
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-21 20:36:50 +0200

    Update 4_extract_emails.py

--- regular_expressions_exercise/4_extract_emails.py
+++ regular_expressions_exercise/4_extract_emails.py
@@ -1,16 +1,29 @@
 import re
 
 text = input()
-pattern = r"(^|(?<=\s))([a-z0-9]+[\.\-\_a-z]*)@([a-z\-]+)(\.[a-z]+)+\b"
-emails = re.finditer(pattern, text)
+pattern = r"\s(([a-z0-9]+[a-z0-9\.\-\_]*)@([a-z\-]+)(\.[a-z]+)+)\b"
+emails = re.findall(pattern, text)
 for email in emails:
-    current_email = email.group()
-    print(current_email)
+    print(email[0])
+
 
 
 
 # import re
-# 
+#
+# text = input()
+# pattern = r"(^|(?<=\s))([a-z0-9]+[\.\-\_a-z]*)@([a-z\-]+)(\.[a-z]+)+\b"
+# emails = re.finditer(pattern, text)
+# for email in emails:
+#     current_email = email.group()
+#     print(current_email)
+
+
+
+
+
+# import re
+#
 # text = input()
 # pattern = r"(^|(?<=\s))([a-z0-9]+[\.\-\_a-z]*)@([a-z\-]+)(\.[a-z]+)+\b"
 # emails = re.finditer(pattern, text)
@@ -20,11 +33,11 @@ for email in emails:
 
 
 # import re
-# 
+#
 # txt = input()
 # pattern = r"(^|(?<=\s))(?P<user>[a-zA-Z0-9]+[._-]?[a-zA-Z0-9]+)@" \
 #           r"(?P<host>[a-zA-Z0-9]+[-]?[a-zA-Z0-9]+\.[a-zA-Z\.]{2,}\b)"
-# 
+#
 # mails = re.finditer(pattern, txt)
 # for mail in mails:
 #     print(f"{mail['user']}@{mail['host']}")
