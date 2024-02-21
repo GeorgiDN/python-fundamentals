6bf3e9bf1e0285b694819b57c56530464418651c
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-21 21:32:13 +0200

    Update 6_extract_the_links.py

--- regular_expressions_exercise/6_extract_the_links.py
+++ regular_expressions_exercise/6_extract_the_links.py
@@ -1,10 +1,24 @@
 import re
 
-text = input()
-while text:
-    pattern = r"((w{3})\.([A-Za-z0-9]+)([A-Za-z0-9\-]+)*(\.[a-z]+)+)"
-    matched_links = re.search(pattern, text)
-    if matched_links:
-        print(matched_links.group())
-    text = input()
-  
+pattern = r"(w{3}.([a-zA-Z0-9-]+)((\.[a-z]+)+))"
+
+while True:
+    line = input()
+    if line == "":
+        break
+
+    link = re.findall(pattern, line)
+    if link:
+        print(link[0][0])
+
+
+        
+# import re
+# 
+# text = input()
+# while text:
+#     pattern = r"((w{3})\.([A-Za-z0-9]+)([A-Za-z0-9\-]+)*(\.[a-z]+)+)"
+#     matched_links = re.search(pattern, text)
+#     if matched_links:
+#         print(matched_links.group())
+#     text = input()
