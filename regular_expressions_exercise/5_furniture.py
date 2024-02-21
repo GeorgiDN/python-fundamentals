0efbd54548a3baaf4b6a43c02cd1f1611e463d6a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-21 21:04:01 +0200

    Update 5_furniture.py

--- regular_expressions_exercise/5_furniture.py
+++ regular_expressions_exercise/5_furniture.py
@@ -8,8 +8,6 @@ while True:
     if text == 'Purchase':
         break
 
-    # pattern = r">>(?P<furniture>[A-Za-z]+)<<(?P<price>\d+(\.\d+)?)!(?P<quantity>\d+)\b"
-    # With this pattern above we have to change match.group with name,not number!
     pattern = r">>([A-Za-z]+)<<(\d+(\.\d+)?)!(\d+)\b"
     matches = re.finditer(pattern, text)
 
@@ -25,3 +23,60 @@ print("Bought furniture:")
 for furniture in furniture_list:
     print(f"{furniture}")
 print(f"Total money spend: {total_price:.2f}")
+
+
+
+# import re
+#
+# total_price = 0
+# furniture_list = []
+#
+# while True:
+#     text = input()
+#     if text == 'Purchase':
+#         break
+#
+#     pattern = r">>(?P<furniture>[A-Za-z]+)<<(?P<price>\d+(\.\d+)?)!(?P<quantity>\d+)\b"
+#     matches = re.finditer(pattern, text)
+#
+#     for match in matches:
+#         furniture_name = match.group("furniture")
+#         price = float(match.group("price"))
+#         quantity = int(match.group("quantity"))
+#
+#         total_price += price * quantity
+#         furniture_list.append(furniture_name)
+#
+# print("Bought furniture:")
+# for furniture in furniture_list:
+#     print(f"{furniture}")
+# print(f"Total money spend: {total_price:.2f}")
+
+
+
+
+# import re
+#
+# total_price = 0
+# furniture_list = []
+#
+# while True:
+#     text = input()
+#     if text == 'Purchase':
+#         break
+#
+#     pattern = r">>(?P<furniture>[A-Za-z]+)<<(?P<price>\d+(\.\d+)?)!(?P<quantity>\d+)\b"
+#     match = re.match(pattern, text)
+#
+#     if match:
+#         furniture_name = match.group("furniture")
+#         price = float(match.group("price"))
+#         quantity = int(match.group("quantity"))
+#
+#         total_price += price * quantity
+#         furniture_list.append(furniture_name)
+#
+# print("Bought furniture:")
+# for furniture in furniture_list:
+#     print(f"{furniture}")
+# print(f"Total money spend: {total_price:.2f}")
