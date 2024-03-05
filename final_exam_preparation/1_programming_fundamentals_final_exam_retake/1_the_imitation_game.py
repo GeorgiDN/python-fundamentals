abbcf6024c0762afec3e4d65218a35be2a36b024
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-05 13:18:03 +0200

    Update 1_the_imitation_game.py

--- final_exam_preparation/1_programming_fundamentals_final_exam_retake/1_the_imitation_game.py
+++ final_exam_preparation/1_programming_fundamentals_final_exam_retake/1_the_imitation_game.py
@@ -1,28 +1,54 @@
-text = input()
+message = input()
+
 while True:
-    command = input()
-    if command == "Decode":
+    data = input().split('|')
+    if data[0] == "Decode":
         break
 
-    parts = command.split("|")
-    action = parts[0]
+    command = data[0]
 
-    if action == 'Move':
-        num_of_letters = int(parts[1])
-        if num_of_letters <= len(text):
-            text = text[num_of_letters:] + text[:num_of_letters]
+    if command == "Move":
+        idx = int(data[1])
+        message = message[idx:] + message[:idx]
 
-    elif action == 'Insert':
-        index = int(parts[1])
-        value = parts[2]
+    elif command == "Insert":
+        index, value = int(data[1]), data[2]
+        message = message[:index] + value + message[index:]
 
-        part1 = text[:index]
-        part2 = text[index:]
-        text = part1 + value + part2
+    elif command == "ChangeAll":
+        substring, replacement = data[1], data[2]
+        message = message.replace(substring, replacement)
 
-    elif action == 'ChangeAll':
-        substring = parts[1]
-        replacement = parts[2]
-        text = text.replace(substring, replacement)
+print(f"The decrypted message is: {message}")
 
-print(f"The decrypted message is: {text}")
+
+
+
+# text = input()
+# while True:
+#     command = input()
+#     if command == "Decode":
+#         break
+
+#     parts = command.split("|")
+#     action = parts[0]
+
+#     if action == 'Move':
+#         num_of_letters = int(parts[1])
+#         if num_of_letters <= len(text):
+#             text = text[num_of_letters:] + text[:num_of_letters]
+
+#     elif action == 'Insert':
+#         index = int(parts[1])
+#         value = parts[2]
+
+#         part1 = text[:index]
+#         part2 = text[index:]
+#         text = part1 + value + part2
+
+#     elif action == 'ChangeAll':
+#         substring = parts[1]
+#         replacement = parts[2]
+#         text = text.replace(substring, replacement)
+
+# print(f"The decrypted message is: {text}")
