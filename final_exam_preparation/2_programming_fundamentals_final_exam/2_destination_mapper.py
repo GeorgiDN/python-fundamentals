63a3086cade710224f075e94377fe4be4f45ebce
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-06 20:40:57 +0200

    Update 2_destination_mapper.py

--- final_exam_preparation/2_programming_fundamentals_final_exam/2_destination_mapper.py
+++ final_exam_preparation/2_programming_fundamentals_final_exam/2_destination_mapper.py
@@ -1,15 +1,51 @@
 import re
 
 text = input()
-
-#pattern = r"([=/])([A-Z][a-zA-Z]{2,})(?=\1)"
-pattern = r"([=/])([A-Z][a-zA-Z]{2,})\1"
+pattern = r"(=|\/)([A-Z][a-zA-Z]{2,})\1"
 matches = re.findall(pattern, text)
 
-valid_destinations = [(match[1]) for match in matches]
-
-destinations = ", ".join(valid_destinations)
-travel_points = sum(len(match) for match in valid_destinations)
-
-print(f"Destinations: {destinations}")
+destinations = [match[1] for match in matches]
+print(f"Destinations: {', '.join(map(str, destinations))}")
+travel_points = sum(len(word) for word in destinations)
 print(f"Travel Points: {travel_points}")
+
+
+
+
+# import re
+#
+#
+# def find_valid_destinations(text):
+#     pattern = r"([=/])([A-Z][a-zA-Z]{2,})\1"
+#     matches = re.findall(pattern, text)
+#
+#     valid_destinations = [(match[1]) for match in matches]
+#
+#     destinations = ", ".join(valid_destinations)
+#     travel_points = sum(len(match) for match in valid_destinations)
+#
+#     print(f"Destinations: {destinations}")
+#     print(f"Travel Points: {travel_points}")
+#
+#
+# data = input()
+# find_valid_destinations(data)
+
+
+
+
+# import re
+#
+# text = input()
+#
+# #pattern = r"([=/])([A-Z][a-zA-Z]{2,})(?=\1)"
+# pattern = r"([=/])([A-Z][a-zA-Z]{2,})\1"
+# matches = re.findall(pattern, text)
+#
+# valid_destinations = [(match[1]) for match in matches]
+#
+# destinations = ", ".join(valid_destinations)
+# travel_points = sum(len(match) for match in valid_destinations)
+#
+# print(f"Destinations: {destinations}")
+# print(f"Travel Points: {travel_points}")
