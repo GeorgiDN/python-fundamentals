8b1e2014791d987c33db1a0a966bdd690303f7a3
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-07 19:40:17 +0200

    Update 2_mirror_words.py

--- final_exam_preparation/3_programming_fundamentals_final_exam_retake/2_mirror_words.py
+++ final_exam_preparation/3_programming_fundamentals_final_exam_retake/2_mirror_words.py
@@ -1,27 +1,66 @@
 import re
 
-info = input()
-pattern = r"([#@])([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})\1"
-matches = re.finditer(pattern, info)
-pairs_found = []
-same_words_reversed = []
 
-for match in matches:
-    first_word = match.group(2)
-    second_word = match.group(3)
-    if first_word[::-1] == second_word:
-        same_words_reversed.append(f"{first_word} <=> {second_word}")
-
-    pairs_found.append(first_word)
-
-if pairs_found:
-    print(f"{len(pairs_found)} word pairs found!")
-elif not pairs_found:
-    print("No word pairs found!")
+def extract_matches(matches, mirror_words, word_pairs_found):
+    for match in matches:
+        word_pairs_found.append(match)
+        first_word = match.group(2)
+        second_word = match.group(3)
+        if first_word == second_word[::-1]:
+            mirror_words.append(f"{first_word} <=> {first_word[::-1]}")
+    return mirror_words, word_pairs_found
 
 
-if same_words_reversed:
-    print("The mirror words are:")
-    print(", ".join(same_words_reversed))
-elif not same_words_reversed:
-    print("No mirror words!")
+def print_result(mirror_words, word_pairs_found):
+    result = ''
+    if not word_pairs_found:
+        result += "No word pairs found!\n"
+    else:
+        result += f"{len(word_pairs_found)} word pairs found!\n"
+
+    if not mirror_words:
+        result += "No mirror words!"
+    else:
+        result += f"The mirror words are:\n"
+        result += f"{', '.join(mirror_words)}"
+
+    return print(''.join(result))
+
+
+text = input()
+pattern = r"([@#])([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})\1"
+matches = re.finditer(pattern, text)
+mirror_words = []
+word_pairs_found = []
+mirror_words, word_pairs_found = extract_matches(matches, mirror_words, word_pairs_found)
+print_result(mirror_words, word_pairs_found)
+
+
+
+
+# import re
+# 
+# text = input()
+# pattern = r"([@#])([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})\1"
+# matches = re.finditer(pattern, text)
+# mirror_words = []
+# word_pairs_found = []
+# 
+# for match in matches:
+#     word_pairs_found.append(match)
+#     first_word = match.group(2)
+#     second_word = match.group(3)
+#     if first_word == second_word[::-1]:
+#         mirror_words.append(f"{first_word} <=> {first_word[::-1]}")
+# 
+# if not word_pairs_found:
+#     print("No word pairs found!")
+# else:
+#     print(f"{len(word_pairs_found)} word pairs found!")
+# 
+# if not mirror_words:
+#     print("No mirror words!")
+# else:
+#     print(f"The mirror words are:\n"
+#           f"{', '.join(mirror_words)}")
+
