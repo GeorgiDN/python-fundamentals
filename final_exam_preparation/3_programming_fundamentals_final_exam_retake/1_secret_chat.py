0ac3960521fa21ee66b3a391ca13193dc12b43b0
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-07 20:58:31 +0200

    Update 1_secret_chat.py

--- final_exam_preparation/3_programming_fundamentals_final_exam_retake/1_secret_chat.py
+++ final_exam_preparation/3_programming_fundamentals_final_exam_retake/1_secret_chat.py
@@ -1,33 +1,88 @@
-text = input()
+def insert_space(text, index):
+    text = text[:index] + " " + text[index:]
+    print(text)
+    return text
 
-while True:
-    command = input()
-    if command == "Reveal":
-        break
 
-    parts = command.split(":|:")
-    action = parts[0]
-
-    if action == "InsertSpace":
-        idx = int(parts[1])
-        text = text[:idx] + ' ' + text[idx:]
+def reverse(text, substring):
+    if substring in text:
+        text = text.replace(substring, '', 1)
+        reversed_substring = substring[::-1]
+        text = text + reversed_substring
         print(text)
+    else:
+        print("error")
+    return text
 
-    elif action == "Reverse":
-        substring = parts[1]
-        if substring in text:
-            text = text.replace(substring, '', 1)
-            text = text + substring[::-1]
-            print(text)
-        else:
-            print("error")
 
-    elif action == "ChangeAll":
-        substring_for_change = parts[1]
-        replacement = parts[2]
-        if substring_for_change in text:
-            while substring_for_change in text:
-                text = text.replace(substring_for_change, replacement)
-        print(text)
+def change_all(text, substring, replacement):
+    text = text.replace(substring, replacement)
+    print(text)
+    return text
 
-print(f"You have a new text message: {text}")
+
+def main():
+    text = input()
+
+    while True:
+        command = input()
+        if command == "Reveal":
+            break
+
+        data = command.split(":|:")
+        current_command = data[0]
+
+        if current_command == "InsertSpace":
+            index = int(data[1])
+            text = insert_space(text, index)
+
+        elif current_command == "Reverse":
+            substring = data[1]
+            text = reverse(text, substring)
+
+        elif current_command == "ChangeAll":
+            substring, replacement = data[1], data[2]
+            text = change_all(text, substring, replacement)
+
+    print(f"You have a new text message: {text}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# text = input()
+
+# while True:
+#     command = input()
+#     if command == "Reveal":
+#         break
+
+#     parts = command.split(":|:")
+#     action = parts[0]
+
+#     if action == "InsertSpace":
+#         idx = int(parts[1])
+#         text = text[:idx] + ' ' + text[idx:]
+#         print(text)
+
+#     elif action == "Reverse":
+#         substring = parts[1]
+#         if substring in text:
+#             text = text.replace(substring, '', 1)
+#             text = text + substring[::-1]
+#             print(text)
+#         else:
+#             print("error")
+
+#     elif action == "ChangeAll":
+#         substring_for_change = parts[1]
+#         replacement = parts[2]
+#         if substring_for_change in text:
+#             while substring_for_change in text:
+#                 text = text.replace(substring_for_change, replacement)
+#         print(text)
+
+# print(f"You have a new text message: {text}")
