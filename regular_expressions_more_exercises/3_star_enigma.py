955627fc1bf60f144b81ac6e5b983329cb3fc43a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-09 15:24:05 +0200

    Update 3_star_enigma.py

--- regular_expressions_more_exercises/3_star_enigma.py
+++ regular_expressions_more_exercises/3_star_enigma.py
@@ -1,32 +1,38 @@
 import re
 
-number_of_messages = int(input())
+
+def print_result(attacked_planets, planets_destroyed):
+    result = ''
+    result += f"Attacked planets: {len(attacked_planets)}\n"
+    if attacked_planets:
+        result += ''.join(([f"-> {planet}\n" for planet in sorted(attacked_planets)]))
+    result += f"Destroyed planets: {len(planets_destroyed)}\n"
+    if planets_destroyed:
+        result += ''.join(([f"-> {planet}\n" for planet in sorted(planets_destroyed)]))
+    return print(result)
+
+
+rows = int(input())
 attacked_planets = []
-destroyed_planets = []
-for message in range(number_of_messages):
+planets_destroyed = []
+
+for message in range(rows):
     current_message = input()
-    encryption_pattern = r"[SsTtAaRr]"
-    decryption_key = len(re.findall(encryption_pattern, current_message))
-    decrypted_message = ""
-    for symbol in current_message:
-        decrypted_message += chr(ord(symbol) - decryption_key)
-    validation_pattern = r"@[A-Za-z]+[^@\-\!\:\>]*\:\d+[^@\-\!\:\>]*\![AD]\![^@\-\!\:\>]*\-\>\d+"
-    validation = re.findall(validation_pattern, decrypted_message)
-    if validation:
-        name_pattern = r"@[A-Za-z]+"
-        attack_pattern = r"\![AD]\!"
-        planet_name = re.findall(name_pattern, decrypted_message)[0][1:]
-        attack_type = re.findall(attack_pattern, decrypted_message)[0][1]
-        if attack_type == "A":
-            attacked_planets.append(planet_name)
-        elif attack_type == "D":
-            destroyed_planets.append(planet_name)
-print(f"Attacked planets: {len(attacked_planets)}")
-if len(attacked_planets) > 0:
-    for planet in sorted(attacked_planets):
-        print(f"-> {planet}")
-print(f"Destroyed planets: {len(destroyed_planets)}")
-if len(destroyed_planets) > 0:
-    for planet in sorted(destroyed_planets):
-        print(f"-> {planet}")
-      
+    encrypt_pattern = r'[star]'
+    decrypt_key = len(re.findall(encrypt_pattern, current_message, re.IGNORECASE))
+    decrypt_message = ''
+    for char in current_message:
+        decrypted_char = chr(ord(char) - decrypt_key)
+        decrypt_message += decrypted_char
+
+    pattern = r'@[A-Za-z]+[^@\-!\:\>]*\:\d+[^@\-!\:\>]![AD]\![^@\-!\:\>]*\-\>\d+'
+    matches = re.findall(pattern, decrypt_message)
+    if matches:
+        planet_name_pattern = r'@[a-zA-z]+'
+        attack_pattern = r'![AD]!'
+        planet_name = re.findall(planet_name_pattern, decrypt_message)[0][1:]
+        attack_type = re.findall(attack_pattern, decrypt_message)[0][1]
+        attacked_planets.append(planet_name) if attack_type == "A" else planets_destroyed.append(planet_name)
+
+print_result(attacked_planets, planets_destroyed)
+
