6b1fe560ebc46459980398f8be5b339f64d85afe
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-09 09:11:21 +0200

    Update word_reverse.py

--- basic_syntax_conditional_statements_and_loops_lab/word_reverse.py
+++ basic_syntax_conditional_statements_and_loops_lab/word_reverse.py
@@ -1,6 +1,41 @@
-word = input()
-reversed_word = ''
+def my_decorator(original_function):
+    def wrapper():
+        word = input()
 
-for i in range(len(word) - 1, -1, -1):
-	reversed_word += word[i]
-print(reversed_word)
+        rev_word = original_function(word)
+        print(rev_word)
+
+    return wrapper
+
+
+@my_decorator
+def word_in_reversed(word):
+    reversed_word = ''
+    for i in range(len(word) - 1, -1, -1):
+        reversed_word += word[i]
+
+    return reversed_word
+
+
+word_in_reversed()
+
+
+
+# print(input()[::-1])
+
+
+
+# number = int(input())
+#
+# for i in reversed(str(number)):
+#     print(i, end='')
+
+
+
+# word = input()
+# reversed_word = ''
+#
+# for i in range(len(word) - 1, -1, -1):
+#     reversed_word += word[i]
+# print(reversed_word)
+
