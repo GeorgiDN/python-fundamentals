bc8a314f7b385c78bbc114abfe8c873badad2ea6
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-27 21:58:32 +0200

    Update 4_match_numbers.py

--- regular_expressions_lab/4_match_numbers.py
+++ regular_expressions_lab/4_match_numbers.py
@@ -1,10 +1,21 @@
 import re
 
-# pattern = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
-pattern = r"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))"
 text = input()
-
-matches = re.finditer(pattern, text)
+pattern = r'(^|\s)(-?\d+(\.\d+)?)(?=$|\s)'
+matches = re.findall(pattern, text)
 
 for match in matches:
-    print(match.group(), end=' ')
+    print(match[1], end=' ')
+
+
+
+# import re
+
+# # pattern = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+# pattern = r"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))"
+# text = input()
+
+# matches = re.finditer(pattern, text)
+
+# for match in matches:
+#     print(match.group(), end=' ')
