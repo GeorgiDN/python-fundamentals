5fbb67e8d424f3e912a89b4d404efce7513c4705
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-04-10 21:19:35 +0300

    Update wolf_in_sheep's_clothing.py

--- basic_syntax_conditional_statements_and_loops_more_exercises/wolf_in_sheep's_clothing.py
+++ basic_syntax_conditional_statements_and_loops_more_exercises/wolf_in_sheep's_clothing.py
@@ -1,11 +1,14 @@
-animals = input()
+queue = input().split(', ')
 
-# Разделяме входната низ на списък по ", "
-queue = animals.split(', ')
-
-if queue[-1] == 'wolf':  # Ако вълкът е най-близо до вас (на последна позиция в списъка)
+if queue[-1] == 'wolf':
     print("Please go away and stop eating my sheep")
 else:
-    wolf_position = queue.index('wolf')  # Намираме позицията на вълка в списъка
-    sheep_number = len(queue) - wolf_position - 1  # Изчисляваме номера на овцете (бройка отзад назад)
+    wolf_position = queue.index('wolf')
+    sheep_number = len(queue) - wolf_position - 1
     print(f"Oi! Sheep number {sheep_number}! You are about to be eaten by a wolf!")
+
+
+# queue = input().split(', ')
+# print("Please go away and stop eating my sheep") if queue[-1] == 'wolf' else print(f"Oi! Sheep number {len(queue) - queue.index('wolf') - 1}! You are about to be eaten by a wolf!")
+
+
