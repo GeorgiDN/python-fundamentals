fa42ef122997ac7c3fb73543d6099a63fdf96803
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-04-27 21:41:28 +0300

    Update next_happy_year.py

--- data_types_and_variables_lab/next_happy_year.py
+++ data_types_and_variables_lab/next_happy_year.py
@@ -1,14 +1,16 @@
-def increase_year(year):
-    return year + 1
+def increase_year(func):
+    def wrapper(*args, **kwargs):
+        year = args[0] + 1
+        while True:
+            year_string = str(year)
+            if len(set(year_string)) == len(year_string):
+                return year
+            year += 1
+    return wrapper
 
 
+@increase_year
 def find_next_happy_year(curr_year):
-    while True:
-        curr_year = increase_year(curr_year)
-        year_string = str(curr_year)
-        if len(set(year_string)) == len(year_string):
-            break
-
     return curr_year
 
 
@@ -16,6 +18,28 @@ current_year = int(input())
 print(find_next_happy_year(current_year))
 
 
+
+##########################################################
+# def increase_year(year):
+#     return year + 1
+
+
+# def find_next_happy_year(curr_year):
+#     while True:
+#         curr_year = increase_year(curr_year)
+#         year_string = str(curr_year)
+#         if len(set(year_string)) == len(year_string):
+#             break
+
+#     return curr_year
+
+
+# current_year = int(input())
+# print(find_next_happy_year(current_year))
+
+
+
+#################################################################
 # year = int(input())
 
 # while True:
